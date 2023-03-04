4db04fc81d524efd3cdf8383ffefe797a8601d5a
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-03-04 19:29:30 +0300

    Update main.ts

--- themes/bloggeri/assets/js/main.ts
+++ themes/bloggeri/assets/js/main.ts
@@ -22,10 +22,10 @@ const RIGHT_SIDE_MATCH_HTML = '</span>';
  * TEMPLATE_TODO: Required. Tell Fuse.js which keys to search on.
  */
 const FUSE_OPTIONS = {
-  keys: ['title', 'country', 'crew', 'vehicle', 'purpose'],
+  keys: ['title', 'category ', 'tag'],
   ignoreLocation: true,
   includeMatches: true,
-  minMatchCharLength: 3
+  minMatchCharLength: 2
 };
 
 let fuse: any;
