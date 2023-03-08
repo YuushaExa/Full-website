ec4d221f44b097e7016a9d5516377b5fe2745deb
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-03-08 15:35:29 +0300

    Update test.js

--- themes/bloggeri/static/js/test.js
+++ themes/bloggeri/static/js/test.js
@@ -1,4 +1,2 @@
- function testFunction() {
-      var imagesrc = document.getElementsByClassName("title-image")[0].src;
-      document.getElementsByTagName("html").innerHTML = '<img src="' + imagesrc + '" >';
-    }
+var bg_url = $('title-image').attr('src')
+document.getElementsByTagName("body")[0].style.backgroundImage = "linear-gradient(-180deg,#1b1d1e8c 300px,#1a1b1e,#201f24 100%),url('" + bg_url + "')";
