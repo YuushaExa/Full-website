7596c307f63046483931bda3b85a7a43f999642b
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-03-08 15:26:48 +0300

    Update test.js

--- themes/bloggeri/static/js/test.js
+++ themes/bloggeri/static/js/test.js
@@ -1,2 +1,2 @@
-var bg_url = document.getElementsByClassName("title-image")[1].src;
+var bg_url = $('#imageContainerId').attr('title-image')
 document.getElementsByTagName("body")[0].style.backgroundImage = "linear-gradient(-180deg,#1b1d1e8c 300px,#1a1b1e,#201f24 100%),url('" + bg_url + "')";
