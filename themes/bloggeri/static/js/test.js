017d7e4cc3090c62ace8d16c895b0a6fcd0a8699
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-03-08 14:16:40 +0300

    Update test.js

--- themes/bloggeri/static/js/test.js
+++ themes/bloggeri/static/js/test.js
@@ -1,1 +1,1 @@
-    $('body').style = "linear-gradient(-180deg,#1b1d1e8c 300px,#1a1b1e,#201f24 100%)";
+    $('body').style.backgroundImage = "linear-gradient(-180deg,#1b1d1e8c 300px,#1a1b1e,#201f24 100%)";
