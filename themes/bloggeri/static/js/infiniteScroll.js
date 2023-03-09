dba283474f785aa04e081dbaf0f7528722abdc3b
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-03-09 21:28:31 +0300

    Update infiniteScroll.js

--- themes/bloggeri/static/js/infiniteScroll.js
+++ themes/bloggeri/static/js/infiniteScroll.js
@@ -1,5 +1,6 @@
 var isf = document.querySelector("aside.isf");
 var mnp = document.querySelector("main");
+var bpg = document.querySelector(".pagination");
 var npl = document.querySelector(".paginator-next-page");
 let infiniteScrollObserver = new IntersectionObserver(entries => {
 entries.forEach(function(entry) {
@@ -8,8 +9,14 @@ if (npl !== null) {
 await fetch(npl.getAttribute("href")).then((response) => response.text()).then((text) => {
 let parserh = new DOMParser();
 let doch = parserh.parseFromString(text, "text/html");
+let pst = doch.querySelector(".content");
+let bpNode = document.adoptNode(pst);
 npl = doch.querySelector(".paginator-next-page"); mnp.appendChild(bpNode);
 });})(); 
+if (document.querySelector(".pagination") !== null) {
+bpg.remove();
+}} else {
+infiniteScrollObserver.unobserve(isf);
 }});},
 { rootMargin: "120px" });
 infiniteScrollObserver.observe(isf);
