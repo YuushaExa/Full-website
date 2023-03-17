3ed344b146ecd2f204271123fe8998ba008cd966
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-03-17 21:55:08 +0000

    nnmmnmmmn

--- public/js/test.js
+++ public/js/test.js
@@ -1,2 +1,2 @@
-var bg_url = document.querySelector(".image-post > img").src;
-document.getElementsByTagName("body")[0].style.backgroundImage = "linear-gradient(-180deg,#1b1d1e8c 300px,#1a1b1e,#201f24 100%),url('" + bg_url + "')";
+var bg_url = document.querySelector(".image-first > img").src.split('&w')[0];
+document.getElementsByTagName("body")[0].style.backgroundImage = "linear-gradient(-180deg,#1b1d1e8c 0,#1a1b1e,#201f24 100%),url('" + bg_url + "')";
