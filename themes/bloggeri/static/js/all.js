922a9da390fb24eeddd6146c0487f7b9d5cc22d0
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-03-28 22:33:36 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -8,8 +8,6 @@ $('.image-post').on('mouseover', function(e){
 });
 
 $(document).ready(function(){
-    $(".content").load("home.html");
-
     $("a").click(function() {
     var page = $(this).attr("href");
     $(".content").load(page);
