9aa6797608ac1055988eb4346c1e8fcd95575b98
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-04-19 00:46:17 +0300

    Update posts-load.js

--- themes/bloggeri/static/js/posts-load.js
+++ themes/bloggeri/static/js/posts-load.js
@@ -1,5 +1,7 @@
   $('.image-post img').each(function(){
   var $this = $(this); 
-  $this.attr('data-src',$this.attr('data-src') + "&w=260");
-  $this.attr("src", $this).removeAttr("data-src");
-})
+    $this.addClass('carousel-cell');
+  $this.attr('data-src',$this.attr('data-src') + "&w=260"); 
+ var source = $(this).attr("data-src");
+$(this).attr("src", source).removeAttr("data-src");
+  })
