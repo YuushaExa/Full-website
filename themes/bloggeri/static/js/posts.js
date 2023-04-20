896d2cdee24def6123d23b5e50e232032e1382e9
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-04-20 18:07:12 +0300

    Update posts.js

--- themes/bloggeri/static/js/posts.js
+++ themes/bloggeri/static/js/posts.js
@@ -3,10 +3,7 @@
   $this.attr('data-src',$this.attr('data-src') + "&w=260");
 })
 
- $('article img').slice(1).each(function() {
-      var img_link =  $(this).attr('data-src').split('&w')[0];
-      $(this).wrap('<a href='+ img_link +' data-fancybox="gallery"></a>')
-    })
+ 
 
 class Lightbox{
   constructor(){
