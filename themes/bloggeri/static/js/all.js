1f91d1ac5d1647fddf813e167b763853b0feb869
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-04-24 15:27:51 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -13,3 +13,45 @@ $("#content-front").click(function() {
            $("#content-front-text").css("opacity", "0");
                               $("#content-front").css("z-index", "-1");
 });
+
+
+class Lightbox{
+  constructor(){
+   this.init()
+  }
+
+  init(){
+   this.container = document.createElement('div');
+   this.container.id = 'lightbox';
+   document.body.appendChild(this.container);
+
+   this.lightboxImg = document.createElement('img');
+   this.container.appendChild(this.lightboxImg);
+
+   this.addListeners();
+  }
+
+  addListeners(){
+   const images = document.querySelectorAll('.game-media img');
+   images.forEach(img => {
+    img.addEventListener('click', ()=> this.galleryImgClicked(img))
+   })
+
+   this.container.addEventListener('click', ()=>{
+    this.hideLightbox()
+   })
+
+   document.addEventListener('keydown', (e) => {
+    if(e.key === 'Escape') this.hideLightbox()
+   })
+  }
+
+  hideLightbox(){
+   this.container.classList.remove('active')
+  }
+
+  galleryImgClicked = (img) => {
+    this.lightboxImg.src = img.src.split('&w')[0];
+    this.container.classList.add('active')
+  }
+}
