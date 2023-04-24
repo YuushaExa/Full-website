eac630c3c74dcb25137f3c7b7bf05dda15ba86ef
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-04-24 15:14:12 +0300

    Update posts-load.js

--- themes/bloggeri/static/js/posts-load.js
+++ themes/bloggeri/static/js/posts-load.js
@@ -4,3 +4,29 @@
  var source = $(this).attr("data-src");
 $(this).attr("src", source).removeAttr("data-src");
   })
+
+const images = document.querySelectorAll('[data-src]');
+const config = {
+  rootMargin: '0px 0px 50px 0px',
+  threshold: 0
+};
+let loaded = 0;
+
+let observer = new IntersectionObserver(function (entries, self) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      preloadImage(entry.target);
+      self.unobserve(entry.target);
+    }
+  });
+}, config);
+
+images.forEach(image => {
+  observer.observe(image);
+});
+
+function preloadImage(img) {
+  const src = img.getAttribute('data-src');
+  if (!src) { return; }
+  img.src = src;
+}             
