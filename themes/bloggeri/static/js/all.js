beb5dd8c522bd4d38d06c323891a4bc65d10865a
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-04-26 00:03:13 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -14,39 +14,9 @@ $("#content-front").click(function() {
                               $("#content-front").css("z-index", "-1");
 });
 
-$("#content-front").click(function(e){
-    $("#content-front-text").show();
-     e.stopPropagation();
-});
 
-$("#content-front-text").click(function(e){
-    e.stopPropagation();
-});
+var modal = document.getElementById("content-front");
 
-$(document).click(function(){
-    $("#content-front-text").hide();
-});
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
