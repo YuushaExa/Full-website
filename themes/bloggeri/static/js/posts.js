aeaba89dbc35c08a63cb2a662c0bd90c7af4c5da
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-05-04 00:28:01 +0300

    Update posts.js

--- themes/bloggeri/static/js/posts.js
+++ themes/bloggeri/static/js/posts.js
@@ -1,8 +1,7 @@
-  $('article img').each(function(){
+  $('article img').slice(1).each(function(){
   var $this = $(this); 
   $this.attr('data-src',$this.attr('data-src') + "&w=260");
 })
-
 
 class Lightbox{
   constructor(){
