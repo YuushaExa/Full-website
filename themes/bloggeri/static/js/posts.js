6409373eeaaf6185a7e57788932ada08985d4f61
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-05-08 01:08:51 +0300

    Update posts.js

--- themes/bloggeri/static/js/posts.js
+++ themes/bloggeri/static/js/posts.js
@@ -3,48 +3,7 @@
   $this.attr('data-src',$this.attr('data-src') + "&w=260");
 })
 
-class Lightbox{
-  constructor(){
-   this.init()
-  }
 
-  init(){
-   this.container = document.createElement('div');
-   this.container.id = 'lightbox';
-   document.body.appendChild(this.container);
-
-   this.lightboxImg = document.createElement('img');
-   this.container.appendChild(this.lightboxImg);
-
-   this.addListeners();
-  }
-
-  addListeners(){
-   const images = document.querySelectorAll('.game-media img');
-   images.forEach(img => {
-    img.addEventListener('click', ()=> this.galleryImgClicked(img))
-   })
-
-   this.container.addEventListener('click', ()=>{
-    this.hideLightbox()
-   })
-
-   document.addEventListener('keydown', (e) => {
-    if(e.key === 'Escape') this.hideLightbox()
-   })
-  }
-
-  hideLightbox(){
-   this.container.classList.remove('active')
-  }
-
-  galleryImgClicked = (img) => {
-    this.lightboxImg.src = img.src.split('&w')[0];
-    this.container.classList.add('active')
-  }
-}
-
-const lightbox = new Lightbox()
 
 $('.game-media img').on('mouseover', function(e){
           var att = $(this).attr('src');  
