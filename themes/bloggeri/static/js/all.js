410017ad12a41447798e69c816aa335a6b750f7e
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-05-23 02:31:59 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -22,7 +22,7 @@ $('.tool-show a').mouseover(function (event) {
  $('.link-pre').css('top',event.pageY + 25 ).css('left',event.pageX + 10 ).hide().load(this.href + ' .image-first, .post-title, .category, .game-info, .game-media ', function (data) {
     }).fadeIn(300).css('transform', 'translate(0, -15px)');
       });
-$('.card').mouseout(function (event) {
+$('.card').mouseleave(function (event) {
   event.preventDefault(); 
   $('.link-pre').css('transform', 'translate(0, +5px)').fadeOut(200, function() {
               $(this).remove();
