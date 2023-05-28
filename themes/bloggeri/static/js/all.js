6c95e3604005bbb9b0f902051b938e262bee9119
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-05-29 02:39:25 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -22,13 +22,9 @@ $('.tool-show a').click(function (event) {
     }).fadeIn(300).css('transform', 'translate(0, -15px)');
       });
 
-var modal = document.getElementsByTagName("body");
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-  event.preventDefault(); 
+$('.tool-show a').mouseout(function (event) {
+      event.preventDefault(); 
   $('.link-pre').css('transform', 'translate(0, +5px)').fadeOut(200, function() {
               $(this).remove();
           });
-}
-}
+   });
