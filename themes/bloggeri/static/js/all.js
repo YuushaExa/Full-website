7464f98587ad006612234b0cf88ccb36c754c040
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-06-03 02:28:32 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -15,13 +15,14 @@ $('.game-info').mouseleave (function (event) {
 $('.tool-show a').click(function (event) {
       event.preventDefault(); 
     $('body').append('<div class="link-pre"></div>');
- $('.link-pre').css('top',event.pageY - 25 ).css('left',event.pageX - 10 ).hide().load(this.href + ' .image-first, .post-title, .category, .game-info, .game-media ', function (data) {
+      $('.link-pre').append('<div class="link-pre1"></div>');
+ $('.link-pre1').css('top',event.pageY - 25 ).css('left',event.pageX - 10 ).hide().load(this.href + ' .image-first, .post-title, .category, .game-info, .game-media ', function (data) {
     }).fadeIn(300).css('transform', 'translate(0, -15px)');
       });
 
-$('#search').mouseleave (function (event) {
+$('.link-pre').mouseleave (function (event) {
       event.preventDefault(); 
- $('.link-pre').css('transform', 'translate(0, +5px)').fadeOut(200, function() {
+ $('.link-pre1').css('transform', 'translate(0, +5px)').fadeOut(200, function() {
               $(this).remove();
 });
 });
