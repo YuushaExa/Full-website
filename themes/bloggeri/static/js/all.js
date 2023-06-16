dad50bde14086808632e6c83c0257f4f43ea0ff8
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-06-16 11:39:17 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -29,3 +29,12 @@ window.onclick = function(event) {
                               $("#content-front").css("z-index", "-1");
 }
 }
+
+  $('.game-media img').each(function(){
+  var $this = $(this); 
+  $this.attr('data-src',$this.attr('data-src') + "&w=260"); 
+ var source = $(this).attr("data-src");
+$(this).attr("src", source).removeAttr("data-src");
+ var img_link = $(this).attr('src').split('&w')[0];
+$(this).wrap('<a href='+ img_link +' data-fancybox="gallery"></a>')
+  })
