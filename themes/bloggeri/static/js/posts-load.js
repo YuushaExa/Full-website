36b28c1976387fbc7262b569bb2280c8dc9230f7
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-06-20 22:10:39 +0300

    Update posts-load.js

--- themes/bloggeri/static/js/posts-load.js
+++ themes/bloggeri/static/js/posts-load.js
@@ -3,4 +3,6 @@
   $this.attr('data-src',$this.attr('data-src') + "&w=260"); 
  var source = $(this).attr("data-src");
 $(this).attr("src", source).removeAttr("data-src");
+ var img_link = $(this).attr('src').split('&w')[0];
+$(this).wrap('<a href='+ img_link +' data-fancybox="gallery"></a>')
   })
