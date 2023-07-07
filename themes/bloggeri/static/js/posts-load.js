91d0c0a7d18fc328346b3faaec6b1be508715b1d
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-07-07 18:06:15 +0300

    Update posts-load.js

--- themes/bloggeri/static/js/posts-load.js
+++ themes/bloggeri/static/js/posts-load.js
@@ -1,8 +1,27 @@
-  $('.game-media img').each(function(){
-  var $this = $(this); 
-  $this.attr('data-src',$this.attr('data-src') + "&w=260"); 
- var source = $(this).attr("data-src");
-$(this).attr("src", source).removeAttr("data-src");
- var img_link = $(this).attr('src').split('&w')[0];
-$(this).wrap('<a href='+ img_link +' data-fancybox="gallery"></a>')
-  })
+  <script>
+const images = document.querySelectorAll('[data-src]');
+const config = {
+  rootMargin: '0px 0px 50px 0px',
+  threshold: 0
+};
+let loaded = 0;
+
+let observer = new IntersectionObserver(function (entries, self) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      preloadImage(entry.target);
+      self.unobserve(entry.target);
+    }
+  });
+}, config);
+
+images.forEach(image => {
+  observer.observe(image);
+});
+
+function preloadImage(img) {
+  const src = img.getAttribute('data-src');
+  if (!src) { return; }
+  img.src = src;
+}               
+</script>
