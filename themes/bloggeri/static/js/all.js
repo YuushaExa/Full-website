456605a88cd6d63e9dade0fb41bc80763890339d
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-07-07 18:39:17 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -19,12 +19,12 @@ $('.tool-show a').mouseover(function (event) {
           $("#content-front").css("z-index", "1").css('background','rgba(0,0,0,.5)');
           $("#content-front-text").css("opacity", "1");
                   $.getScript("/js/posts-load.js"); 
-  var $this = $(this); 
+  var $this = $('.game-media img'); 
   $this.attr('data-src',$this.attr('data-src') + "&w=260"); 
- var source = $(this).attr("data-src");
-$(this).attr("src", source).removeAttr("data-src");
- var img_link = $(this).attr('src').split('&w')[0];
-$(this).wrap('<a href='+ img_link +' data-fancybox="gallery"></a>')
+ var source = $('.game-media img').attr("data-src");
+$('.game-media img').attr("src", source).removeAttr("data-src");
+ var img_link = $('.game-media img').attr('src').split('&w')[0];
+$('.game-media img').wrap('<a href='+ img_link +' data-fancybox="gallery"></a>')
 
 });
 var modal = document.getElementById("content-front");
