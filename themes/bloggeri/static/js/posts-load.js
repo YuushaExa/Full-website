25be347ac0b2445d4e7731307eaa8cf4344fe6e5
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-07-30 20:44:39 +0300

    Update posts-load.js

--- themes/bloggeri/static/js/posts-load.js
+++ themes/bloggeri/static/js/posts-load.js
@@ -3,11 +3,11 @@
   $this.attr('data-src',$this.attr('data-src') + "&h=200");
 })
 
- $('.game-media img').each(function(lazy) {
+ $('.game-media img').each(function(lazy1) {
       var img_link =  $(this).attr('data-src').split('&h')[0];
       $(this).wrap('<a href='+ img_link +' data-fancybox="gallery"></a>')
     })
-$('.game-media img').each(function(lazy){
+$('.game-media img').each(function(lazy2){
 
  var source = $(this).attr("data-src");
 $(this).attr("src", source).removeAttr("data-src");
