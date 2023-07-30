40255312bdc57ad89f517f4dfefbe81f9b71dff7
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-07-30 18:45:35 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -19,6 +19,7 @@ $('.tool-show a').mouseover(function (event) {
           $("#content-front").css("z-index", "1").css('background','rgba(0,0,0,.5)');
           $("#content-front-text").css("opacity", "1");
                   $.getScript("/js/posts-load.js");
+      lazy();
 });
 var modal = document.getElementById("content-front");
 window.onclick = function(event) {
