55f6fd0526c1ab88997414c75486571fdd2be540
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-07-31 19:49:53 +0300

    Update posts-load.js

--- themes/bloggeri/static/js/posts-load.js
+++ themes/bloggeri/static/js/posts-load.js
@@ -1,9 +1,1 @@
-const callback = (entries, observer) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      console.log('Элемент пересёк границу области и всё ещё соприкасается с ней!')
-
-      observer.unobserve(entry.target)
-    }
-  })
-}
+m
