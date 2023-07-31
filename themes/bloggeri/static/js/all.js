481e728b2453db492ff537a32cb57644259c7676
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-08-01 02:01:54 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -18,17 +18,7 @@ $('.tool-show a').mouseover(function (event) {
     });
           $("#content-front").css("z-index", "1").css('background','rgba(0,0,0,.5)');
           $("#content-front-text").css("opacity", "1");
-      $('.game-media img').each(function(){
-  var $this = $(this); 
-  $this.attr('data-src',$this.attr('data-src') + "&h=200");
-
-      var img_link =  $(this).attr('data-src').split('&h')[0];
-      $(this).wrap('<a href='+ img_link +' data-fancybox="gallery"></a>')
-
- var source = $(this).attr("data-src");
-$(this).attr("src", source).removeAttr("data-src");
-
-  });
+      $.getScript("/js/posts-load.js");
 });
 
 var modal = document.getElementById("content-front");
