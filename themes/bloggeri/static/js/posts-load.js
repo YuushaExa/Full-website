2a62b79a5ac480854f6523b3c2f18192e829a7d5
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-07-31 19:34:54 +0300

    Update posts-load.js

--- themes/bloggeri/static/js/posts-load.js
+++ themes/bloggeri/static/js/posts-load.js
@@ -1,18 +1,3 @@
-
-let observer = new IntersectionObserver(function (entries, self) {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      preloadImage(entry.target);
-      self.unobserve(entry.target);
-        entry.target.classList.add("visible");
-    }
-  });
-}, config);
-
-images.forEach(image => {
-  observer.observe(image);
-});
-
 function preloadImage(img) {
   const src = img.getAttribute('data-src');
   if (!src) { return; }
