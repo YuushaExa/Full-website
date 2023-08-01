27d9f66b470c8d1c05cb69f7024de7fa999f9249
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-08-01 23:29:03 +0300

    Update posts-load.js

--- themes/bloggeri/static/js/posts-load.js
+++ themes/bloggeri/static/js/posts-load.js
@@ -1,7 +1,1 @@
-$('.game-media img').each(function(){
-  var $this = $(this); 
-  $this.attr('data-src',$this.attr('data-src') + "&h=200");
-
-      var img_link =  $(this).attr('data-src').split('&h')[0];
-      $(this).wrap('<a href='+ img_link +' data-fancybox="gallery"></a>')
-  });
+m
