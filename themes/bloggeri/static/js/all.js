6c62bad468801ea8cee9e5a0c50f7b230810b3f2
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-08-02 02:15:20 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -29,7 +29,7 @@ if (event.target == modal) {
 
 
 
-$("body").on("click", "#content-front-text", function() {
+$(".tool-show a").on("mouseover", "#content-front-text", function() {
   $('.game-media img').each(function(){
       var img_link =  $(this).attr('data-src').split('&h')[0];
       $(this).wrap('<a href='+ img_link +' data-fancybox="gallery"></a>')
