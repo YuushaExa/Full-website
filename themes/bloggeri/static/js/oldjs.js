c7a39653ec74a696790c7ec0cd03c46450442b0d
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-08-07 14:41:33 +0300

    Update oldjs.js

--- themes/bloggeri/static/js/oldjs.js
+++ themes/bloggeri/static/js/oldjs.js
@@ -6,3 +6,11 @@ $(this).attr("src", source).removeAttr("data-src");
  var img_link = $(this).attr('src').split('&w')[0];
 $(this).wrap('<a href='+ img_link +' data-fancybox="gallery"></a>')
   })
+
+$("body").on("mouseover", "#content-front-text", function() {
+ $('.game-media img:not(.gallery)').each(function(){
+      var img_link =  $(this).attr('data-src').split('&h')[0];
+      $(this).wrap('<a href='+ img_link +' data-fancybox="gallery"></a>')
+       $('.game-media img').addClass('gallery');
+  });
+});
