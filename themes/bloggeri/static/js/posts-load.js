40f924baf4463d9afc1a0f201b4f523728a9ead7
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-08-12 19:10:43 +0300

    Update posts-load.js

--- themes/bloggeri/static/js/posts-load.js
+++ themes/bloggeri/static/js/posts-load.js
@@ -13,10 +13,3 @@ function openCity(evt, cityName) {
 }
 
 document.getElementById("defaultOpen").click();
-
-
-
-$('.game-media img').each(function(){
-      var img_link =  $(this).attr('data-src').split('&h')[0];
-      $(this).wrap('<a href='+ img_link +' data-fancybox="gallery"></a>')
-  });
