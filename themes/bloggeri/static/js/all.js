a3fb8fc6c8358c06f96f1b0332c3c7a0b49c4d32
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-08-14 00:06:36 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -20,8 +20,7 @@ $('.tool-show a').mouseover(function (event) {
 });
 
 $('.card-image img').mouseover(function (event) { 
-var bg_url = $(this).prop('src').split('&w')[0];
-bg_url.attr('src',bg_url.attr('src') + "&w=260");
+var bg_url = $(this).prop('src').split('&w')[0].join('&w=300');
 document.querySelector("#content-front-text").style.backgroundImage = "linear-gradient(0deg, rgba(255, 0, 150, 0.3), rgba(255, 0, 150, 0.3)),url('" + bg_url + "')";
 });
       
