6cf6222c281310a04649b706ea8ad3e98000c44e
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-08-15 22:56:16 +0300

    Update posts-load.js

--- themes/bloggeri/static/js/posts-load.js
+++ themes/bloggeri/static/js/posts-load.js
@@ -15,7 +15,7 @@ function openCity(evt, cityName) {
 
 document.getElementById("defaultOpen").click();
 
-var bg_url = $('#content-front-text .image-first img').prop('src').replace(/&w=200&h=300/i, "&w=300&h=175");
+var bg_url = $('#content-front-text .image-first img').prop('src');
 
 var colorThief = new ColorThief();
 $('.image-first').each(function() {
