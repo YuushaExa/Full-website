02f28527c746a9c51b51c6502d4f2179a63b25e9
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-08-16 00:41:27 +0300

    Update posts-load.js

--- themes/bloggeri/static/js/posts-load.js
+++ themes/bloggeri/static/js/posts-load.js
@@ -21,9 +21,6 @@ $('.image-first').each(function() {
     var thumb = $(this);
     thumb.find('img').each(function() {
         thisColor = colorThief.getColor(this);
-        thumb.parent().find('#game-description').css({
-            background: 'rgb('+ thisColor +')'
-         })
       thumb.parent().find('#content-front-text').css({
             background: 'linear-gradient(0deg, rgba('+ thisColor +', 0.3), rgba('+ thisColor +', 0.3)),url(' + bg_url + ')'
          })
