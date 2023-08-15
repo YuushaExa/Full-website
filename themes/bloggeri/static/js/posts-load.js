06f8a73296f94ca4919548904558544242fd14fd
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-08-15 20:53:57 +0300

    Update posts-load.js

--- themes/bloggeri/static/js/posts-load.js
+++ themes/bloggeri/static/js/posts-load.js
@@ -18,31 +18,4 @@ document.getElementById("defaultOpen").click();
 var bg_url = $('#content-front-text .image-first img').prop('src').replace(/&w=200&h=300/i, "&w=300&h=175");
 document.querySelector("#content-front-text").style.backgroundImage = "linear-gradient(0deg, rgba(255, 0, 150, 0.3), rgba(255, 0, 150, 0.3)),url('" + bg_url + "')";
 
-const cards = document.querySelectorAll("#content-front-text");
-const colorThief = new ColorThief();
-cards.forEach((card) => {
-  const wrapper = card.querySelectorAll(".image-first")[0];
-  const img = wrapper.querySelectorAll("img")[0];
-  wrapper.style.cssText += `background-image: url(${img.src})`;
-});
-window.onload = () => {
-  detectColors();
-};
-function detectColors() {
-  cards.forEach((card) => {
-    const wrapper = card.querySelectorAll(".image-first")[0];
-    const cta = card.querySelectorAll("#game-description")[0];
-    const img = wrapper.querySelectorAll("img")[0];
-    const rgb = getColorData(img);
-    let lum = tinycolor(rgb).getLuminance();
-    cta.style.cssText += `background-color: ${rgb}; color: ${
-      lum <= 0.4 ? `#FFF` : `#000`
-    }; text-shadow: 0px 0px 3px ${
-      lum <= 0.4 ? `#000` : `#FFF`
-    }`;
-  });
-}
-function getColorData(img) {
-  let rgbArray = colorThief.getColor(img);
-  return `rgb(${rgbArray[0]}, ${rgbArray[1]}, ${rgbArray[2]})`;
-}
+
