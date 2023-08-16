0e2c364998cb57d85af90034d0591be804742cf0
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-08-17 01:47:56 +0300

    Update posts-load.js

--- themes/bloggeri/static/js/posts-load.js
+++ themes/bloggeri/static/js/posts-load.js
@@ -28,5 +28,5 @@ $('.image-first').each(function() {
     });
 });
 
-  var bg_url = $('#content-front-text #game-media img').prop('src')[0].replace(/&w=200&h=300/i, "&w=300&h=175");
+  var bg_url = $('#content-front-text #game-media img').prop('src')[0];
  document.querySelector("#game-info").style.backgroundImage = "linear-gradient(0deg, rgb(0 0 0 / 40%), rgb(109 109 109 / 40%)),url('" + bg_url + "')";
