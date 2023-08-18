9d44b667b7bf2eccea8dacb66a67eb202ef9ebcd
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-08-19 01:25:38 +0300

    Update posts-load.js

--- themes/bloggeri/static/js/posts-load.js
+++ themes/bloggeri/static/js/posts-load.js
@@ -14,17 +14,6 @@ function openCity(evt, cityName) {
 
 document.getElementById("defaultOpen").click();
 
-var colorThief = new ColorThief();
-$('#content-front-text #game-media img').each(function() {
-    var thumb = $(this);
-    thumb.find('img:nth-child(3)').each(function() {
-        thisColor = colorThief.getColor(this);
-        thumb.parent().find('#game-description').css({
-            background: 'rgb('+ thisColor +')'
-         })
-    });
-});
-
 var numItems = $('#content-front-text #game-media img').length
 $('.tablinks:nth-child(3)').text(numItems);
 
@@ -34,3 +23,13 @@ $('.tablinks:nth-child(3)').text(numItems);
    var bg_url = $('#content-front-text #game-media img:nth-child(3)').data('src').replace(/&w=200&h=300/i, "&w=300&h=175");
  document.querySelector("#content-front-text").style.backgroundImage = "linear-gradient(0deg, rgb(0 0 0 / 40%), rgb(109 109 109 / 40%)),url('" + bg_url + "')";
 
+var colorThief = new ColorThief();
+$('#content-front-text').each(function() {
+    var thumb = $(this);
+    thumb.find('img').each(function() {
+        thisColor = colorThief.getColor(this);
+        thumb.parent().find('#game-description').css({
+            background: 'rgb('+ thisColor +')'
+         })
+    });
+});
