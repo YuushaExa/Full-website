f4bebd394f805623992704c78a72b4c475696c6d
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-08-21 01:14:54 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -19,7 +19,7 @@ $('.game-info').mouseleave (function (event) {
           });
 });
 
-$('body').on("click", "#game-preview a", function (event) { 
+$('#game-preview a').mouseover(function (event) { 
     event.preventDefault();
        $('#content-front-text').load(this.href + ' .post-title, .tab, .image-first, #game-media, #game-info, #game-description, .game-links ', function (data) {
     });
