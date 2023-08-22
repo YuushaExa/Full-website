87fd5ccfdb7d08e750f8f8e3fd842ed58e55fc0c
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-08-22 16:39:13 +0300

    Update posts-load.js

--- themes/bloggeri/static/js/posts-load.js
+++ themes/bloggeri/static/js/posts-load.js
@@ -15,11 +15,11 @@ function openCity(evt, cityName) {
 document.getElementById("defaultOpen").click();
 
 var numItems = $('#content-front-text #game-media img').length
-$('.tablinks:nth-child(3)').text(numItems);
+$('.tablinks').text(numItems);
 
- var bg_url = $('#content-front-text #game-media img:nth-child(3)').data('src').replace(/&w=200&h=300/i, "&w=300&h=175");
- document.querySelector(".tablinks:nth-child(3)").style.backgroundImage = "linear-gradient(0deg, rgb(0 0 0 / 40%), rgb(109 109 109 / 40%)),url('" + bg_url + "')";
-   var bg_url = $('#content-front-text #game-media img:nth-child(3)').data('src').replace(/&w=200&h=300/i, "&w=300&h=175");
+ var bg_url = $('#content-front-text #game-media img').data('src').replace(/&w=200&h=300/i, "&w=300&h=175");
+ document.querySelector(".tablinks").style.backgroundImage = "linear-gradient(0deg, rgb(0 0 0 / 40%), rgb(109 109 109 / 40%)),url('" + bg_url + "')";
+   var bg_url = $('#content-front-text #game-media img').data('src').replace(/&w=200&h=300/i, "&w=300&h=175");
  document.querySelector("#content-front-text").style.backgroundImage = "linear-gradient(0deg, rgb(0 0 0 / 40%), rgb(109 109 109 / 40%)),url('" + bg_url + "')";
 
 var colorThief = new ColorThief();
