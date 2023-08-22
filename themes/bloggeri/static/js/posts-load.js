84da28733232fb3452da16326fbd7bbd490bf58a
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-08-22 16:14:47 +0300

    Update posts-load.js

--- themes/bloggeri/static/js/posts-load.js
+++ themes/bloggeri/static/js/posts-load.js
@@ -1,21 +1,4 @@
-  document.addEventListener("DOMContentLoaded", function () {
-    let images = document.querySelectorAll("img[data-src]");
-    function loadImagesLazily(e) {
-      for (let i = 0; i < images.length; i++) {
-        let rect = images[i].getBoundingClientRect();
-        if (images[i].hasAttribute("data-src")
-          && rect.bottom > 0 && rect.top < window.innerHeight
-          && rect.right > 0 && rect.left < window.innerWidth) {
-          images[i].setAttribute("src", images[i].getAttribute("data-src"));
-          images[i].removeAttribute("data-src");
-        }
-      }
-    };
-
-    window.addEventListener('scroll', loadImagesLazily);
-    window.addEventListener('resize', loadImagesLazily);
-    loadImagesLazily();
-  });
+ loadImagesLazily();
 
 var numItems = $('#content-front-text #game-media img').length
 $('.tablinks:nth-child(3)').text(numItems);
