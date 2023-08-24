d4c930fc6c8882968e3e0a0cd08b4a40db407f1e
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-08-25 01:31:17 +0300

    Update posts-load.js

--- themes/bloggeri/static/js/posts-load.js
+++ themes/bloggeri/static/js/posts-load.js
@@ -31,3 +31,8 @@ $('#content-front-text').each(function() {
          })
     });
 });
+
+$("button").click(function(){
+    var iframe = $("#myiFrame");
+    iframe.attr("src", iframe.data("src")); 
+});
