eaa4972a15d66dae027f0b7130e861896191e1b2
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-09-06 03:04:02 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -81,10 +81,10 @@ $("body").on("click", "#button1", function() {
 $('.Platforms a').mouseover(function (event) {
       event.preventDefault(); 
     $('.Platforms').append('<div class="link-pre1"></div><div class="link-pre"></div>');
-    $('.link-pre').css('top',event.pageY - 25 ).css('left',event.pageX - 10 ).hide().load("/search" + ' .PC ', function (data) {
+    $('.link-pre').css('top',event.pageY - 25 ).css('left',event.pageX - 10 ).hide().load("/search" + ' window.location.hash ', function (data) {
     }).fadeIn(300).css('transform', 'translate(0, -15px)');
 });
-$('.Platforms a').mouseleave (function (event) {
+$('.link-pre1').mouseleave (function (event) {
   event.preventDefault(); 
   $('.link-pre').css('transform', 'translate(0, +5px)').fadeOut(200, function() {
               $(this).remove();
