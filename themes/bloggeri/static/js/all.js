ebda0be95aa5598df64859f8ce6d97d3a09e9ba7
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-09-06 14:45:54 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -80,8 +80,8 @@ $("body").on("click", "#button1", function() {
 
 $('.Platforms a').mouseover(function (event) {
       event.preventDefault(); 
-    $('.Platforms').append('</div><div class="link-pre"></div>');
-    $('.link-pre').css('top',event.pageY - 25 ).css('left',event.pageX - 10 ).hide().load('https://yuushaexa.github.io/search/' + ' #PC ', function (data) {
+    $('.Platforms').append('</div><div class="link-pre"></div>'); 
+    $('.link-pre').css('top',event.pageY - 25 ).css('left',event.pageX - 10 ).hide().load('/search' + ' #PC ', function (data) {
     }).fadeIn(300).css('transform', 'translate(0, -15px)');
 });
 $('.Platforms a').mouseleave (function (event) {
