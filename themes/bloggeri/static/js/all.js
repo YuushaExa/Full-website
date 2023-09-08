7a464a59f77d445eaa7800d454bd12d5d40f0d8a
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-09-09 00:26:03 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -81,7 +81,6 @@ $("body").on("click", "#button1", function() {
 $('.Platforms a').mouseover(function (event) {
       event.preventDefault(); 
     $('.Platforms').append('<div class="link-pre"></div>'); 
-     var k = $('.Platforms a').text;
     $('.link-pre').hide().load(this.href + ' .content ', function (data) {
     }).fadeIn(300).css('transform', 'translate(0, -15px)');
 });
