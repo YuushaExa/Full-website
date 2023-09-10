94c2f0aef089a28bb0859c2d80c0efd50b9c47fb
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-09-11 02:28:19 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -39,7 +39,7 @@ $('.link-pre').mouseleave (function (event) {
 });
 
 
-var modal = $("body");
+var modal = $(".container");
 window.onclick = function(event) {
 if (event.target == modal) {
   $(".link-pre").empty();
