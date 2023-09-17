a5a005c1b29e059a850b8ca2c13f306980c9d233
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-09-18 00:58:53 +0300

    Update test.js

--- themes/bloggeri/static/js/test.js
+++ themes/bloggeri/static/js/test.js
@@ -1,2 +1,2 @@
-var bg_url = document.querySelector("#Jsontest").src.split('&w')[0];
+var bg_url = document.querySelector("img").data-src.split('&w')[0];
 document.querySelector(".cover").style.backgroundImage = "linear-gradient(-180deg,#1b1d1e8c 0,#1a1b1e,#201f24 100%),url('" + bg_url + "')";
