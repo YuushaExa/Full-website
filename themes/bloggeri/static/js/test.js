a49155869c40b22f4826190dc2d98cb5ad65ae67
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-09-25 00:33:27 +0300

    Update test.js

--- themes/bloggeri/static/js/test.js
+++ themes/bloggeri/static/js/test.js
@@ -1,2 +1,13 @@
+var colorThief = new ColorThief();
+$('.cover-front').each(function() {
+    var thumb = $(this);
+    thumb.find('img').each(function() {
+        thisColor = colorThief.getColor(this);
+        thumb.parent().find('.cover').css({
+            background: 'rgb('+ thisColor +')'
+         })
+    });
+});
+
 var bg_url = document.querySelector("img").src.split('&w')[0];
-document.querySelector(".cover").style.backgroundImage = "linear-gradient(-180deg,#1b1d1e8c 0,#1a1b1e,#201f24 100%),url('" + bg_url + "')";
+document.querySelector(".cover").style.backgroundImage = "linear-gradient(-180deg, rgb(27 29 30 / 55%) 0px, rgb('"+ thisColor +"') 100%),url('" + bg_url + "')";
