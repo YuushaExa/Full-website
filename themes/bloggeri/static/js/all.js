4e2363142eb500fc0cf0faf0ff78c41f9e65f206
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-09-25 00:42:14 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -11,6 +11,8 @@ $('.cover-front').each(function() {
 $('html').css({
             background: 'rgb('+ thisColor +')'
          });
+var bg_url = document.querySelector("img").src.split('&w')[0];
+document.querySelector(".cover").style.backgroundImage = "linear-gradient(-180deg, rgb(27 29 30 / 55%) 0px, rgb('" + thisColor + "') 100%),url('" + bg_url + "')";
 
 $('.btn5').click(function () { 
   $.ajax({
