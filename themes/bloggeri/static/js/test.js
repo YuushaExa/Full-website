6b958cca75661bd4d67621c912011b36dc0a841f
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-09-25 00:37:52 +0300

    Update test.js

--- themes/bloggeri/static/js/test.js
+++ themes/bloggeri/static/js/test.js
@@ -1,2 +1,2 @@
 var bg_url = document.querySelector("img").src.split('&w')[0];
-document.querySelector(".cover").style.backgroundImage = "linear-gradient(-180deg, rgb(27 29 30 / 55%) 0px, rgb('+ thisColor +') 100%),url('" + bg_url + "')";
+document.querySelector(".cover").style.backgroundImage = "linear-gradient(-180deg, rgb(27 29 30 / 55%) 0px, rgb(11 11 11) 100%),url('" + bg_url + "')";
