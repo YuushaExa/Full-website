857319d8594390e70067130c626e871ad7ee5591
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-09-26 02:17:22 +0300

    Update oldjs.js

--- themes/bloggeri/static/js/oldjs.js
+++ themes/bloggeri/static/js/oldjs.js
@@ -33,3 +33,31 @@ $("body").on("click", ".tablinks:nth-child(3)", function() {
 $(this).attr("src", source).removeAttr("data-src");
   });
 });
+
+
+$("body").on("mouseover", "#game-preview a", function(event) {
+    event.preventDefault();
+       $('#content-front-text').load(this.href + ' .post-title, .tab, .image-first, #game-media, #game-info, #game-description, .game-links, #GBinfo, #Jsontest ', function (data) {
+    });
+  var url = this;
+  history.pushState({}, "", url);
+          $("#content-front").css("z-index", "1").css('background','rgba(0,0,0,.5)');
+          $("#content-front-text").css("opacity", "1");   
+});
+
+$("body").on("mouseover", "#game-preview a", function(event) {
+{setTimeout(function(){
+    jQuery.getScript("/js/posts-load.js");
+}, 300)};
+});
+
+var modal = document.getElementById("content-front");
+window.onclick = function(event) {
+if (event.target == modal) {
+  $("#content-front-text").empty();
+    var url = "https://yuushaexa.github.io/";
+  history.pushState({}, "", url);
+       $("#content-front-text").css("opacity", "0");
+                              $("#content-front").css("z-index", "-1").css('background','unset');
+}
+}
