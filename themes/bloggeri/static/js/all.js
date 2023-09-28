929cd22e4eda5b109ad5d1cd869a92cbdcccb447
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-09-29 01:03:20 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -7,7 +7,7 @@ document.querySelectorAll('.card a').forEach(link => {
       .then(html => {
         const parser = new DOMParser();
         const doc = parser.parseFromString(html, 'text/html');
-        const postTitle = doc.querySelector('.post-title','.content').innerHTML;
+        const postTitle = doc.querySelector('.content').innerHTML;
         
         document.getElementById('content-front-text').innerHTML = postTitle;
       })
