b3d9e15628b6db968885c9e444947045a59343f5
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-10-01 00:22:03 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -27,7 +27,6 @@ contentFrontTextElement.style.opacity = "1";
   }
 });
 
-
 document.body.addEventListener("click", async function(event) {
   const target = event.target;
   if (target.matches(".navbar-brand a")) {
