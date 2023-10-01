548ecb21cb01dc995b68c95292a76f3e0450cb4c
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-10-01 04:02:55 +0300

    Create lightboxAI.js

---
+++ themes/bloggeri/static/js/lightboxAI.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+  var galleryImages = document.querySelectorAll('.gallery img');
+
+  // Attach click event listener to each image
+  galleryImages.forEach(function(image) {
+    image.addEventListener('click', function() {
+      openLightbox(image.src);
+    });
+  });
+};
+
+function openLightbox(imageSrc) {
+  var lightbox = document.getElementById('lightbox');
+  var lightboxImg = document.getElementById('lightbox-img');
+
+  // Set the image source
+  lightboxImg.src = imageSrc;
+
+  // Show the lightbox
+  lightbox.classList.remove('hidden');
+}
+
+function closeLightbox() {
+  var lightbox = document.getElementById('lightbox');
+
+  // Hide the lightbox
+  lightbox.classList.add('hidden');
+}
