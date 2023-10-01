a7d7b7166cd8b94f4555ec0193baf1342589eff0
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-10-01 17:58:03 +0300

    Update lightboxAI.js

--- themes/bloggeri/static/js/lightboxAI.js
+++ themes/bloggeri/static/js/lightboxAI.js
@@ -1,46 +1,25 @@
 window.onload = function() {
   var galleryImages = document.querySelectorAll('.gallery img');
-  var currentIndex = 0;
-
   // Attach click event listener to each image
-  galleryImages.forEach(function(image, index) {
+  galleryImages.forEach(function(image) {
     image.addEventListener('click', function() {
-      currentIndex = index;
       openLightbox(image.src);
     });
   });
-}
+};
 function openLightbox(imageSrc) {
   var lightbox = document.getElementById('lightbox');
   var lightboxImg = document.getElementById('lightbox-img');
-
   // Set the image source
   lightboxImg.src = imageSrc;
-
   // Show the lightbox
   lightbox.classList.remove('hidden');
-
     document.documentElement.style.overflow = 'hidden';
 };
-
 function closeLightbox() {
   var lightbox = document.getElementById('lightbox');
-
   // Hide the lightbox
   lightbox.classList.add('hidden');
     document.documentElement.style.overflow = 'auto';
 
 };
-
-function prevSlide() {
-  currentIndex = (currentIndex - 1 + images.length) % galleryImages.length;
-  var lightboxImg = document.getElementById('lightbox-img');
-  lightboxImg.src = galleryImages[currentIndex];
-}
-
-
-function nextSlide() {
-    currentIndex = (currentIndex + 1) % galleryImages.length;
-    var lightboxImg = document.getElementById('lightbox-img');
-    lightboxImg.src = galleryImages[currentIndex].src;
-  }
