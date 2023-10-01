df8fe486d935e73603bb1c96deda38d9416468f3
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-10-01 22:39:56 +0300

    Update lightboxAI.js

--- themes/bloggeri/static/js/lightboxAI.js
+++ themes/bloggeri/static/js/lightboxAI.js
@@ -1,6 +1,5 @@
    var galleryImages = document.querySelectorAll('.gallery img');
   var currentIndex = 0;
-  var preloadedImages = [];
 
   // Attach click event listener to each image
   galleryImages.forEach(function(image, index) {
@@ -10,37 +9,32 @@
     });
   });
 
-  function preloadImage(index) {
-    var image = new Image();
-    image.src = galleryImages[index].src;
-    preloadedImages.push(image);
-
-     var image = document.getElementById('lightbox-img');
-image.src = image.getAttribute('data-src');
-image.removeAttribute('data-src');
-  }
-
   function openLightbox(imageSrc) {
     var lightbox = document.getElementById('lightbox');
     var lightboxImg = document.getElementById('lightbox-img');
     lightboxImg.src = imageSrc;
     lightbox.classList.remove('hidden');
     document.documentElement.style.overflow = 'hidden';
-
-    preloadImage((currentIndex + 1) % galleryImages.length); // Preload next image
-    preloadImage((currentIndex - 1 + galleryImages.length) % galleryImages.length); // Preload previous image
   }
 
   function nextSlide() {
     currentIndex = (currentIndex + 1) % galleryImages.length;
     var lightboxImg = document.getElementById('lightbox-img');
     lightboxImg.src = galleryImages[currentIndex].src;
+
+       var image = document.getElementById('lightbox-img');
+image.src = image.getAttribute('data-src');
+image.removeAttribute('data-src');
   }
 
   function prevSlide() {
     currentIndex = (currentIndex - 1 + galleryImages.length) % galleryImages.length;
     var lightboxImg = document.getElementById('lightbox-img');
     lightboxImg.src = galleryImages[currentIndex].src;
+
+       var image = document.getElementById('lightbox-img');
+image.src = image.getAttribute('data-src');
+image.removeAttribute('data-src');
   }
 
   
