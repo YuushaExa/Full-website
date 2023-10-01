46d20ff7a232f8eb26a1390e63f37a109ccb12f8
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-10-01 18:02:09 +0300

    Update lightboxAI.js

--- themes/bloggeri/static/js/lightboxAI.js
+++ themes/bloggeri/static/js/lightboxAI.js
@@ -1,25 +1,44 @@
 window.onload = function() {
   var galleryImages = document.querySelectorAll('.gallery img');
+  var currentIndex = 0;
+
   // Attach click event listener to each image
-  galleryImages.forEach(function(image) {
+  galleryImages.forEach(function(image, index) {
     image.addEventListener('click', function() {
+      currentIndex = index;
       openLightbox(image.src);
     });
   });
-};
-function openLightbox(imageSrc) {
-  var lightbox = document.getElementById('lightbox');
-  var lightboxImg = document.getElementById('lightbox-img');
-  // Set the image source
-  lightboxImg.src = imageSrc;
-  // Show the lightbox
-  lightbox.classList.remove('hidden');
-    document.documentElement.style.overflow = 'hidden';
-};
-function closeLightbox() {
-  var lightbox = document.getElementById('lightbox');
-  // Hide the lightbox
-  lightbox.classList.add('hidden');
-    document.documentElement.style.overflow = 'auto';
 
+  function openLightbox(imageSrc) {
+    var lightbox = document.getElementById('lightbox');
+    var lightboxImg = document.getElementById('lightbox-img');
+    lightboxImg.src = imageSrc;
+    lightbox.classList.remove('hidden');
+    document.documentElement.style.overflow = 'hidden';
+  }
+
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % galleryImages.length;
+    var lightboxImg = document.getElementById('lightbox-img');
+    lightboxImg.src = galleryImages[currentIndex].src;
+  }
+
+  function closeLightbox() {
+    var lightbox = document.getElementById('lightbox');
+    lightbox.classList.add('hidden');
+    document.documentElement.style.overflow = 'auto';
+  }
+
+  // Attach click event listener to the next button
+  var nextButton = document.querySelector('.next');
+  nextButton.addEventListener('click', function() {
+    nextSlide();
+  });
+
+  // Attach click event listener to the close button
+  var closeButton = document.querySelector('.close');
+  closeButton.addEventListener('click', function() {
+    closeLightbox();
+  });
 };
