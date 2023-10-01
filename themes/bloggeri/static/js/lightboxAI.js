f7a5c82c2fa048e14b028a99ca2705005cc9a8f8
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-10-01 04:26:19 +0300

    Update lightboxAI.js

--- themes/bloggeri/static/js/lightboxAI.js
+++ themes/bloggeri/static/js/lightboxAI.js
@@ -53,3 +53,64 @@ window.onload = function() {
     }
   });
 };
+
+var startX = 0;
+var startY = 0;
+var currentX = 0;
+var currentY = 0;
+var isDragging = false;
+
+window.onload = function() {
+  var galleryImages = document.querySelectorAll('.gallery img');
+  var lightboxImg = document.getElementById('lightbox-img');
+
+  // Attach click event listener to each image
+  galleryImages.forEach(function(image) {
+    image.addEventListener('click', function() {
+      openLightbox(image.src);
+    });
+  });
+
+  // Attach touchstart, touchmove, touchend, mousedown, mousemove, and mouseup event listeners to the lightbox image
+  lightboxImg.addEventListener('touchstart', handleStart);
+  lightboxImg.addEventListener('touchmove', handleMove, { passive: true });
+  lightboxImg.addEventListener('touchend', handleEnd);
+  lightboxImg.addEventListener('mousedown', handleStart);
+  lightboxImg.addEventListener('mousemove', handleMove);
+  lightboxImg.addEventListener('mouseup', handleEnd);
+};
+
+function handleStart(event) {
+  startX = event.clientX || event.touches[0].clientX;
+  startY = event.clientY || event.touches[0].clientY;
+  currentX = startX;
+  currentY = startY;
+  isDragging = true;
+}
+
+function handleMove(event) {
+  if (!isDragging) return;
+
+  var newX = event.clientX || event.touches[0].clientX;
+  var newY = event.clientY || event.touches[0].clientY;
+
+  var deltaX = newX - currentX;
+  var deltaY = newY - currentY;
+
+  var lightboxImg = document.getElementById('lightbox-img');
+  lightboxImg.style.transform = 'translate(' + deltaX + 'px, ' + deltaY + 'px)';
+
+  currentX = newX;
+  currentY = newY;
+}
+
+function handleEnd() {
+  if (!isDragging) return;
+
+  isDragging = false;
+
+  var lightboxImg = document.getElementById('lightbox-img');
+  lightboxImg.style.transform = '';
+}
+
+// Rest of the code remains the same...
