e1dc254c95041f8a70bddd27049f520c60d1a7f0
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-10-01 18:28:57 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -171,6 +171,25 @@ closeLightbox();
 };
 };
 
+  var galleryImages = document.querySelectorAll('.gallery img');
+  var currentIndex = 0;
+
+  // Attach click event listener to each image
+  galleryImages.forEach(function(image, index) {
+    image.addEventListener('click', function() {
+      currentIndex = index;
+      openLightbox(image.src);
+    });
+  });
+
+  function openLightbox(imageSrc) {
+    var lightbox = document.getElementById('lightbox');
+    var lightboxImg = document.getElementById('lightbox-img');
+    lightboxImg.src = imageSrc;
+    lightbox.classList.remove('hidden');
+    document.documentElement.style.overflow = 'hidden';
+  }
+
   function nextSlide() {
     currentIndex = (currentIndex + 1) % galleryImages.length;
     var lightboxImg = document.getElementById('lightbox-img');
