15411a0c35c0bd5d1a6dcdf4d68713735fd88ada
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-10-03 01:19:06 +0300

    Update lightboxAI.js

--- themes/bloggeri/static/js/lightboxAI.js
+++ themes/bloggeri/static/js/lightboxAI.js
@@ -1,15 +1,19 @@
 var galleryImages = document.querySelectorAll('.gallery img');
 var currentIndex = 0;
 var lightboxImages = [];
+var isLoadingImage = false; // Flag to track if an image is currently being loaded
+
 galleryImages.forEach(function(image) {
   var imgLink = image.getAttribute('data-src').split('&w')[0] + "&w=1920&fit=inside&we";
   var link = document.createElement('a');
   link.href = imgLink;
   link.setAttribute('data-fancybox', 'gallery');
+
   image.parentNode.insertBefore(link, image);
   link.appendChild(image);
   lightboxImages.push(link);
 });
+
 lightboxImages.forEach(function(image, index) {
   image.addEventListener('click', function(event) {
     event.preventDefault();
@@ -17,113 +21,111 @@ lightboxImages.forEach(function(image, index) {
     openLightbox(image.href);
   });
 });
- 
-function loadImage(imageSrc, callback) {
-  var image = new Image();
-  image.onload = function() {
-    callback(null, image);
-  };
-  image.onerror = function() {
-    callback(new Error('Failed to load image'));
-  };
-  image.src = imageSrc;
-}
 
 function openLightbox(imageSrc) {
   var lightbox = document.getElementById('lightbox');
   var lightboxImg = document.getElementById('lightbox-img');
   var loadingText = document.getElementById('loading-text');
+  var isImageLoaded = false;
 
-  // Show loading text
+  if (isLoadingImage) {
+    return; // Return early if an image is currently being loaded
+  }
+
+  isLoadingImage = true; // Set the flag to true when starting to load an image
   loadingText.style.display = 'block';
-  // Hide image until loaded
   lightboxImg.style.display = 'none';
 
-  loadImage(imageSrc, function(error, image) {
-    if (error) {
-      console.error(error);
-      return;
-    }
-
-    // Hide loading text
+  lightboxImg.src = imageSrc;
+  lightboxImg.onload = function() {
+    isImageLoaded = true;
     loadingText.style.display = 'none';
-    // Show image
-    lightboxImg.src = image.src;
     lightboxImg.style.display = 'block';
+    isLoadingImage = false; // Set the flag to false when the image has finished loading
+  };
 
-    lightbox.classList.remove('hidden');
-    document.body.classList.add('lightbox-open');
-    document.documentElement.style.overflow = 'hidden';
+  lightbox.classList.remove('hidden');
+  document.body.classList.add('lightbox-open');
+  document.documentElement.style.overflow = 'hidden';
+  setTimeout(function() {
+    preloadNextPrevImages();
+  }, 10000);
 
-    setTimeout(function() {
-      preloadNextPrevImages();
-    }, 10000);
-
-      var lightbox = document.getElementById('lightbox');
+  var lightbox = document.getElementById('lightbox');
   if (lightbox) {
     lightbox.addEventListener('wheel', handleMouseWheel, { passive: true });
   }
-  });
 }
+
 function preloadNextPrevImages() {
   var nextIndex = (currentIndex + 1) % lightboxImages.length;
   var nextImg = new Image();
   nextImg.src = lightboxImages[nextIndex].href;
 }
+
 function nextSlide() {
+  currentIndex = (currentIndex + 1) % lightboxImages.length;
+  var lightboxImg = document.getElementById('lightbox-img');
+  var loadingText = document.getElementById('loading-text');
+  var isImageLoaded = false;
+
+  if (isLoadingImage) {
+    return; // Return early if an image is currently being loaded
+  }
+
+  isLoadingImage = true;
+  loadingText.style.display = 'block';
+  lightboxImg.style.display = 'none';
+  lightboxImg.src = lightboxImages[currentIndex].href;
+  lightboxImg.onload = function() {
+    isImageLoaded = true;
+    loadingText.style.display = 'none';
+    lightboxImg.style.display = 'block';
+    document.querySelector('.next').disabled = false;
+    document.querySelector('.prev').disabled = false;
+    isLoadingImage = false;
+  };
+
+  document.querySelector('.next').disabled = true;
+  document.querySelector('.prev').disabled = true;
   setTimeout(function() {
-    currentIndex = (currentIndex + 1) % lightboxImages.length;
-    var lightboxImg = document.getElementById('lightbox-img');
-    var loadingText = document.getElementById('loading-text');
-    var isImageLoaded = false; // Flag to track if the current image has finished loading
-
-    // Show loading text
-    loadingText.style.display = 'block';
-    // Hide image until loaded
-    lightboxImg.style.display = 'none';
-
-    // Set image source
-    lightboxImg.src = lightboxImages[currentIndex].href;
-    // Wait for image to load
-    openLightbox(imageSrc);
-    // Preload next/previous images
-    setTimeout(function() {
-      preloadNextPrevImages();
-    }, 10000);
-  }, 1000);
+    preloadNextPrevImages();
+  }, 10000);
 }
 
 function prevSlide() {
-  // Disable next/prev buttons while loading
+  currentIndex = (currentIndex - 1 + lightboxImages.length) % lightboxImages.length;
+  var lightboxImg = document.getElementById('lightbox-img');
+  var loadingText = document.getElementById('loading-text');
+  var isImageLoaded = false;
+
+  if (isLoadingImage) {
+    return; // Return early if an image is currently being loaded
+  }
+
+  isLoadingImage = true;
+  loadingText.style.display = 'block';
+  lightboxImg.style.display = 'none';
+  lightboxImg.src = lightboxImages[currentIndex].href;
+  lightboxImg.onload = function() {
+    isImageLoaded = true;
+    loadingText.style.display = 'none';
+    lightboxImg.style.display = 'block';
+    document.querySelector('.next').disabled = false;
+    document.querySelector('.prev').disabled = false;
+    isLoadingImage = false;
+  };
+
   document.querySelector('.next').disabled = true;
   document.querySelector('.prev').disabled = true;
-
   setTimeout(function() {
-    currentIndex = (currentIndex - 1 + lightboxImages.length) % lightboxImages.length;
-    var lightboxImg = document.getElementById('lightbox-img');
-    var loadingText = document.getElementById('loading-text');
-    var isImageLoaded = false; // Flag to track if the current image has finished loading
-
-    // Show loading text
-    loadingText.style.display = 'block';
-    // Hide image until loaded
-    lightboxImg.style.display = 'none';
-
-    // Set image source
-    lightboxImg.src = lightboxImages[currentIndex].href;
-    // Wait for image to load
- openLightbox(imageSrc);
-
-    // Preload next/previous images
-    setTimeout(function() {
-      preloadNextPrevImages();
-    }, 10000);
-  }, 1000);
+    preloadNextPrevImages();
+  }, 10000);
 }
 
 window.addEventListener('keydown', function(event) {
   var lightbox = document.getElementById('lightbox');
-  if (lightbox && lightbox.classList.contains('hidden') === false) {
+  if (lightbox && !lightbox.classList.contains('hidden')) {
     if (event.key === 'ArrowRight') {
       nextSlide();
     } else if (event.key === 'ArrowLeft') {
@@ -131,9 +133,10 @@ window.addEventListener('keydown', function(event) {
     }
   }
 });
+
 function handleMouseWheel(event) {
   var lightbox = document.getElementById('lightbox');
-  if (lightbox && lightbox.classList.contains('hidden') === false) {
+  if (lightbox && !lightbox.classList.contains('hidden')) {
     var delta = event.deltaY || event.detail || (-event.wheelDelta);
     if (delta > 0) {
       nextSlide();
@@ -142,6 +145,7 @@ function handleMouseWheel(event) {
     }
   }
 }
+
 function closeLightbox() {
   var lightbox = document.getElementById('lightbox');
   lightbox.classList.add('hidden');
