2d558e494f459fcfafb482e68e98b0d0f79f9b4d
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-10-03 02:49:40 +0300

    Update lightboxAI.js

--- themes/bloggeri/static/js/lightboxAI.js
+++ themes/bloggeri/static/js/lightboxAI.js
@@ -86,10 +86,19 @@ document.addEventListener('touchend', function(event) {
 function handleSwipe() {
   var swipeThreshold = 50; // Adjust this value as needed
   var deltaX = touchEndX - touchStartX;
- 
-  if (deltaX > swipeThreshold) {
-    prevSlide();
-  } else if (deltaX < -swipeThreshold) {
-    nextSlide();
+  var deltaY = touchEndY - touchStartY; // Added deltaY calculation
+
+  if (Math.abs(deltaX) > Math.abs(deltaY)) {
+    // Horizontal swipe
+    if (deltaX > swipeThreshold) {
+      prevSlide();
+    } else if (deltaX < -swipeThreshold) {
+      nextSlide();
+    }
+  } else {
+    // Vertical swipe
+    if (deltaY > swipeThreshold) {
+      closeLightbox(); // Close the lightbox on vertical swipe down
+    }
   }
 }
