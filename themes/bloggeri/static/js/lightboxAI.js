5409493e2861a330fcacbf34f57aa4b21d6ad5c6
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-10-03 02:15:07 +0300

    Update lightboxAI.js

--- themes/bloggeri/static/js/lightboxAI.js
+++ themes/bloggeri/static/js/lightboxAI.js
@@ -1,57 +1,51 @@
-const galleryImages = document.querySelectorAll('.gallery img');
-const lightbox = document.getElementById('lightbox');
-const lightboxImg = document.getElementById('lightbox-img');
-const lightboxCloseBtn = document.getElementById('lightbox-close');
-const lightboxNextBtn = document.getElementById('lightbox-next');
-const lightboxPrevBtn = document.getElementById('lightbox-prev');
-
-let currentIndex = 0;
-const lightboxImages = [];
-
+var galleryImages = document.querySelectorAll('.gallery img');
+var currentIndex = 0;
+ 
 // Wrap each image with an <a> tag and set the data-fancybox attribute
-galleryImages.forEach(function (image, index) {
-  const imgLink = image.getAttribute('data-src').split('&w')[0] + '&w=1920&fit=inside&we';
-  const link = document.createElement('a');
+galleryImages.forEach(function(image) {
+  var imgLink = image.getAttribute('data-src').split('&w')[0] + "&w=1920&fit=inside&we";
+  var link = document.createElement('a');
   link.href = imgLink;
   link.setAttribute('data-fancybox', 'gallery');
-  link.appendChild(image);
   image.parentNode.insertBefore(link, image);
-  lightboxImages.push(link);
-
-  // Attach click event listener to each image
-  link.addEventListener('click', function (event) {
+  link.appendChild(image);
+});
+ 
+var lightboxImages = document.querySelectorAll('a[data-fancybox="gallery"]');
+ 
+// Attach click event listener to each image
+lightboxImages.forEach(function(image, index) {
+  image.addEventListener('click', function(event) {
     event.preventDefault();
     currentIndex = index;
-    openLightbox(link.href);
+    openLightbox(image.href);
   });
 });
-
-// Attach click event listeners to lightbox buttons
-lightboxCloseBtn.addEventListener('click', closeLightbox);
-lightboxNextBtn.addEventListener('click', nextSlide);
-lightboxPrevBtn.addEventListener('click', prevSlide);
-
+ 
 function openLightbox(imageSrc) {
+  var lightbox = document.getElementById('lightbox');
+  var lightboxImg = document.getElementById('lightbox-img');
   lightboxImg.src = imageSrc;
   lightbox.classList.remove('hidden');
+document.body.classList.add('lightbox-open');
   document.documentElement.style.overflow = 'hidden';
 }
-
-function switchSlide() {
-  lightboxImg.src = lightboxImages[currentIndex].href;
-}
-
+ 
 function nextSlide() {
   currentIndex = (currentIndex + 1) % lightboxImages.length;
-  switchSlide();
+  var lightboxImg = document.getElementById('lightbox-img');
+  lightboxImg.src = lightboxImages[currentIndex].href;
 }
-
+ 
 function prevSlide() {
   currentIndex = (currentIndex - 1 + lightboxImages.length) % lightboxImages.length;
-  switchSlide();
+  var lightboxImg = document.getElementById('lightbox-img');
+  lightboxImg.src = lightboxImages[currentIndex].href;
 }
-
+ 
 function closeLightbox() {
+  var lightbox = document.getElementById('lightbox');
   lightbox.classList.add('hidden');
+  document.body.classList.remove('lightbox-open');
   document.documentElement.style.overflow = 'auto';
 }
