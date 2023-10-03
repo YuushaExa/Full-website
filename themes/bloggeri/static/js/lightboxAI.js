bd8eb56777554b8dd679a4b2ec726f3c762fbf25
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-10-04 01:16:11 +0300

    Update lightboxAI.js

--- themes/bloggeri/static/js/lightboxAI.js
+++ themes/bloggeri/static/js/lightboxAI.js
@@ -5,7 +5,7 @@ var openedImageCounter = document.getElementById('opened-image-counter');
 var totalImageCounter = document.getElementById('total-image-counter');
 updateCounters();
   var loadingBarC = document.getElementById('loading-bar-container');
-
+var loadingText = document.getElementById('loading-text');
  
 function updateCounters() {
   openedImageCounter.textContent = currentIndex + 1;
