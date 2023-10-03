c21366102f9d3ce54a8241ecb0b45564dad0fab0
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-10-04 00:14:00 +0300

    Update lightboxAI.js

--- themes/bloggeri/static/js/lightboxAI.js
+++ themes/bloggeri/static/js/lightboxAI.js
@@ -1,55 +1,51 @@
-// Variable declarations
 var galleryImages = document.querySelectorAll('.gallery img');
 var currentIndex = 0;
 var totalImages = galleryImages.length;
 var openedImageCounter = document.getElementById('opened-image-counter');
 var totalImageCounter = document.getElementById('total-image-counter');
-var lightboxImages = document.querySelectorAll('[data-fancybox="gallery"]');
-
-// Function to update image counters
+updateCounters();
+ 
 function updateCounters() {
   openedImageCounter.textContent = currentIndex + 1;
   totalImageCounter.textContent = totalImages;
 }
-
-// Gallery setup
-galleryImages.forEach(function (image) {
+ 
+galleryImages.forEach(function(image) {
+  var imgLink = image.getAttribute('data-src').split('&w')[0] + "&w=1920&fit=inside&we";
   var link = document.createElement('a');
-  link.href = image.getAttribute('data-src') + '?w=1200&h=800&fit=crop'; // Example of image manipulation using query parameters
+  link.href = imgLink;
+  link.setAttribute('data-fancybox', 'gallery');
   image.parentNode.insertBefore(link, image);
   link.appendChild(image);
 });
-
-// Lightbox setup
-lightboxImages.forEach(function (image) {
-  image.addEventListener('click', function (e) {
-    e.preventDefault();
-    currentIndex = Array.from(lightboxImages).indexOf(image);
+ 
+var lightboxImages = document.querySelectorAll('a[data-fancybox="gallery"]');
+ 
+lightboxImages.forEach(function(image, index) {
+  image.addEventListener('click', function(event) {
+    event.preventDefault();
+    currentIndex = index;
     openLightbox(image.href);
   });
 });
-
-// Function to update the loading bar
-function updateLoadingBar(progress, loadedKB, totalKB) {
-  var loadingBar = document.getElementById('loading-bar');
-  var loadingProgress = document.getElementById('loading-progress');
-  
-  loadingBar.style.width = progress + '%';
-  loadingProgress.textContent = loadedKB + ' KB / ' + totalKB + ' KB';
-}
-
-// Function to open the lightbox
+ 
 function openLightbox(imageSrc) {
   var lightbox = document.getElementById('lightbox');
   var lightboxImg = document.getElementById('lightbox-img');
-  var loadingText = document.getElementById('loading-text');
-  
-  lightbox.style.display = 'flex';
-  document.body.classList.add('lightbox-open');
-  
+ 
+var loadingText = document.getElementById('loading-text');
+ 
+ var loadingBar = document.getElementById('loading-bar');
+  var loadingProgress = document.getElementById('loading-progress');
+
   lightboxImg.style.display = 'none'; // Hide the image initially
   loadingText.style.display = 'block'; // Show the loading text
-  
+  loadingBar.style.width = '0%'; // Reset the loading bar width
+ 
+  var loadingTimeout = setTimeout(function() {
+    loadingText.style.display = 'block'; // Show the loading text
+  }, 1000);
+
   var xhr = new XMLHttpRequest();
   xhr.open('GET', imageSrc, true);
   xhr.responseType = 'blob';
@@ -59,101 +55,146 @@ function openLightbox(imageSrc) {
       var loadedKB = Math.round(event.loaded / 1024); // Convert bytes to KB
       var totalKB = Math.round(event.total / 1024); // Convert bytes to KB
       var progress = (event.loaded / event.total) * 100;
-      updateLoadingBar(progress, loadedKB, totalKB);
+      loadingBar.style.width = progress + '%'; // Update the loading bar width based on the progress
+      loadingProgress.textContent = loadedKB + ' KB / ' + totalKB + ' KB'; // Update the loading progress text
     }
   };
 
   xhr.onload = function(event) {
     if (xhr.status === 200) {
+      loadingBar.style.width = '100%'; // Set the loading bar width to 100% when the image is fully loaded
       var imageUrl = URL.createObjectURL(xhr.response);
       lightboxImg.src = imageUrl;
-      lightboxImg.style.display = 'block'; // Show the image
-      loadingText.style.display = 'none'; // Hide the loading text
-      updateLoadingBar(100, 0, 0); // Update loading bar to 100% when the image is fully loaded
+      loadingProgress.textContent = ''; // Clear the loading progress text
     }
   };
 
   xhr.send();
-  
+ 
+  // Add a load event listener to the image
+  lightboxImg.addEventListener('load', function() {
+    clearTimeout(loadingTimeout); // Cancel the loading text timeout
+    lightboxImg.style.display = 'block'; // Show the image
+    loadingText.style.display = 'none'; // Hide the loading text
+  });
+ 
+  lightboxImg.src = imageSrc;
+  lightbox.classList.remove('hidden');
+document.body.classList.add('lightbox-open');
+  document.documentElement.style.overflow = 'hidden';
   updateCounters();
 }
-
-// Function to navigate to the next slide
+ 
 function nextSlide() {
   currentIndex = (currentIndex + 1) % lightboxImages.length;
-  openLightbox(lightboxImages[currentIndex].href);
+  var lightboxImg = document.getElementById('lightbox-img');
+ 
+  var loadingText = document.getElementById('loading-text');
+ 
+    lightboxImg.style.display = 'none'; // Hide the image initially
+ 
+  var loadingTimeout = setTimeout(function() {
+    loadingText.style.display = 'block'; // Show the loading text
+  }, 1000);
+ 
+  lightboxImg.addEventListener('load', function() {
+    clearTimeout(loadingTimeout); // Cancel the loading text timeout
+    lightboxImg.style.display = 'block'; // Show the image
+    loadingText.style.display = 'none'; // Hide the loading text
+  });
+ 
+  lightboxImg.src = lightboxImages[currentIndex].href;
+   updateCounters();
 }
-
-// Function to navigate to the previous slide
+ 
 function prevSlide() {
   currentIndex = (currentIndex - 1 + lightboxImages.length) % lightboxImages.length;
-  openLightbox(lightboxImages[currentIndex].href);
+  var lightboxImg = document.getElementById('lightbox-img');
+ 
+  var loadingText = document.getElementById('loading-text');
+ 
+  lightboxImg.style.display = 'none'; // Hide the image initially
+ 
+  var loadingTimeout = setTimeout(function() {
+    loadingText.style.display = 'block'; // Show the loading text
+  }, 1000);
+ 
+  // Add a load event listener to the image
+  lightboxImg.addEventListener('load', function() {
+    clearTimeout(loadingTimeout); // Cancel the loading text timeout
+    lightboxImg.style.display = 'block'; // Show the image
+    loadingText.style.display = 'none'; // Hide the loading text
+  });
+ 
+  lightboxImg.src = lightboxImages[currentIndex].href;
+   updateCounters();
 }
-
-// Function to close the lightbox
+ 
 function closeLightbox() {
   var lightbox = document.getElementById('lightbox');
-  lightbox.style.display = 'none';
+  lightbox.classList.add('hidden');
   document.body.classList.remove('lightbox-open');
   document.documentElement.style.overflow = 'auto';
 }
-
-// Event listeners
-
-// Mouse wheel event listener
-window.addEventListener('wheel', function (e) {
-  if (document.body.classList.contains('lightbox-open')) {
-    e.preventDefault();
-    if (e.deltaY > 0) {
+ 
+document.addEventListener('wheel', function(event) {
+  if (!lightbox.classList.contains('hidden')) {
+    event.preventDefault();
+    if (event.deltaY > 0) {
       nextSlide();
     } else {
       prevSlide();
     }
   }
 });
-
-// Keyboard event listener
-window.addEventListener('keydown', function (e) {
-  if (document.body.classList.contains('lightbox-open')) {
-    if (e.key === 'ArrowRight') {
+ 
+window.addEventListener('keydown', function(event) {
+  if (lightbox && !lightbox.classList.contains('hidden')) {
+    if (event.key === 'ArrowRight') {
       nextSlide();
-    } else if (e.key === 'ArrowLeft') {
+    } else if (event.key === 'ArrowLeft') {
       prevSlide();
     }
   }
 });
-
-// Touch event listeners
-var touchstartX = 0;
-var touchendX = 0;
-
-window.addEventListener('touchstart', function (e) {
-  touchstartX = e.touches[0].clientX;
+ 
+var touchStartX = 0;
+var touchEndX = 0;
+var touchStartY = 0;
+var touchEndY = 0;
+ 
+document.addEventListener('touchstart', function(event) {
+  touchStartY = event.touches[0].clientY;
 });
-
-window.addEventListener('touchend', function (e) {
-  touchendX = e.changedTouches[0].clientX;
+ 
+document.addEventListener('touchend', function(event) {
+  touchEndY = event.changedTouches[0].clientY;
   handleSwipe();
 });
-
+ 
+document.addEventListener('touchstart', function(event) {
+  touchStartX = event.touches[0].clientX;
+});
+ 
+document.addEventListener('touchend', function(event) {
+  touchEndX = event.changedTouches[0].clientX;
+  handleSwipe();
+});
+ 
 function handleSwipe() {
-  if (document.body.classList.contains('lightbox-open')) {
-    if (touchendX < touchstartX){
-      nextSlide();
-    } else if (touchendX > touchstartX) {
-      prevSlide();
-    }
+  var swipeThreshold = 50; // Adjust this value as needed
+  var deltaX = touchEndX - touchStartX;
+  var deltaY = touchEndY - touchStartY;
+ 
+  if (deltaY > swipeThreshold) {
+    closeLightbox();
+  } else if (deltaY < -swipeThreshold) {
+    closeLightbox()
+  }
+ 
+  if (deltaX > swipeThreshold) {
+    prevSlide();
+  } else if (deltaX < -swipeThreshold) {
+    nextSlide();
   }
 }
-
-// Lightbox close button event listener
-var closeBtn = document.getElementById('close-btn');
-closeBtn.addEventListener('click', closeLightbox);
-
-// Lightbox next button event listener
-var nextBtn = document.getElementById('next-btn');
-nextBtn.addEventListener('click', nextSlide);
-
-// Lightbox previous button event listener
-var prevBtn = document.getElementById('prev-btn');
-prevBtn.addEventListener('click', prevSlide);
