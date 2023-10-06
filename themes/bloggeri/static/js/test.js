f23182fd152dbf30966069c070e6caef0dce8a8f
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-10-07 02:30:50 +0300

    Update test.js

--- themes/bloggeri/static/js/test.js
+++ themes/bloggeri/static/js/test.js
@@ -3,14 +3,15 @@ var coverFrontElements = document.querySelectorAll('.cover-front');
 coverFrontElements.forEach(function(coverFront) {
     var thumb = coverFront;
     var imgElements = thumb.querySelectorAll('img');
-    imgElements.forEach(function(img) {
-        var thisColor = colorThief.getColor(img);
+    
+    // Get the first image element
+    var firstImg = imgElements[0];
+    
+    // Attach a load event listener to the first image
+    firstImg.addEventListener('load', function() {
+        var thisColor = colorThief.getColor(firstImg);
         var coverElement = thumb.parentElement.querySelector('.cover');
         coverElement.style.background = 'rgb(' + thisColor.join(',') + ')';
+        document.querySelector('html').style.background = 'rgb(' + thisColor.join(',') + ')';
     });
 });
-document.querySelector('html').style.background = 'rgb(' + thisColor.join(',') + ')';
-
-var bg = 'rgb('+ thisColor +')'
-var bg_url = document.querySelector("img").src.split('&w')[0];
-document.querySelector(".cover").style.backgroundImage = "linear-gradient(-180deg,#1b1d1e8c 0," + bg + "," + bg + " 100%),url('" + bg_url + "')";
