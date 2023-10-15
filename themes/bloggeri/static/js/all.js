3d4d99cf1d8216cc2785e67ac832bd4ea6d3eeab
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-10-16 00:46:29 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -20,35 +20,19 @@ saveButtons.forEach((button) => {
 function displaySavedCards() {
   const cardContainer = document.getElementById('cardContainer');
   cardContainer.innerHTML = ''; // Clear the container before populating it again
-
   // Loop through the local storage items
   for (let i = 0; i < localStorage.length; i++) {
     const title = localStorage.key(i); // Get the title (key) of the stored card
     const content = localStorage.getItem(title); // Get the content (value) of the stored card
-
     // Create a new card element
     const card = document.createElement('div');
     card.classList.add('card');
     card.innerHTML = content;
 
-    // Create a delete button for the card
-    const deleteButton = document.createElement('button');
-    deleteButton.classList.add('deleteButton');
-    deleteButton.textContent = 'Delete';
-    deleteButton.addEventListener('click', function() {
-      // Remove the card from local storage and re-display the saved cards
-      localStorage.removeItem(title);
-      displaySavedCards();
-    });
-
-    // Append the delete button to the card
-    card.appendChild(deleteButton);
-
     // Append the card to the container
     cardContainer.appendChild(card);
   }
 }
-
 // Call the function to display the saved cards when the page loads
 displaySavedCards();
 
