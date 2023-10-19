decccc73657e7dcfa59460a9269907bf53c966f7
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-10-19 19:05:27 +0300

    Update infiniteScroll.js

--- themes/bloggeri/static/js/infiniteScroll.js
+++ themes/bloggeri/static/js/infiniteScroll.js
@@ -23,30 +23,13 @@ function loadNextPage() {
       tempContainer.innerHTML = html;
       const nextPageContent = tempContainer.querySelector("main").innerHTML;
       currentPagePaginationContainer.insertAdjacentHTML('beforeend', nextPageContent);
-
-      // Update the next page link on the current page
-      let currentPageNextLink = document.querySelector(".paginator-next-page");
-      let newNextLink = tempContainer.querySelector(".paginator-next-page");
-      if (newNextLink) {
-        currentPageNextLink.setAttribute("href", newNextLink.getAttribute("href"));
-        nextPage = newNextLink.getAttribute("href");  // Update nextPage with the new link
-        console.log("pagination: Updated next page link!");
-      } else {
-        currentPageNextLink.parentNode?.removeChild(currentPageNextLink);
-        nextPage = null;  // Set nextPage to null when there are no more pages
-        console.log("pagination: Removed next page anchor!");
-      }
-
-      // Update history
-      let state = {
-        "status": "pagination: New list items added",
-        "previousPage": window.location.pathname + window.location.search,
-        "currentPage": nextPage
-      };
-      history.pushState(state, "", nextPage);
-      console.log("pagination: New history pushed - ", state);
-
+      nextPage = tempContainer.querySelector(".paginator-next-page")?.href || null;
       loading = false;
+
+      // Update browser history with the new page URL
+      const nextPageTitle = document.title;
+      const nextPageURL = nextPage || window.location.href;
+      window.history.pushState({ page: nextPageURL }, nextPageTitle, nextPageURL);
     })
     .catch(error => {
       console.error(error);
