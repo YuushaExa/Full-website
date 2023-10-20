bdc97e4aea6f206faaa1e8559df3b03e61cd3f17
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-10-21 02:31:58 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -4,9 +4,9 @@ function checkLocalStorage(card, toggleButton) {
   const title = card.querySelector('.title.is-4').textContent;
 
   if (localStorage.getItem(title)) {
-   toggleButton.style.fill = 'rgb(4 252 14 / 80%);';
-  } else {
     toggleButton.style.fill = 'rgb(255 255 255 / 80%)';
+  } else {
+    toggleButton.style.fill = 'rgb(4 252 14 / 80%);';
   }
 }
 
@@ -25,11 +25,11 @@ function toggleLocalStorage(event) {
 
   if (localStorage.getItem(title)) {
     localStorage.removeItem(title);
-    toggleButton.style.fill = 'rgb(255 255 255 / 80%)';
+    toggleButton.style.fill = 'rgb(4 252 14 / 80%)';
   } else {
     const content = card.innerHTML;
     localStorage.setItem(title, content);
-   toggleButton.style.fill = 'rgb(4 252 14 / 80%);';
+    toggleButton.style.fill = 'rgb(255 255 255 / 80%)';
   }
 }
 
