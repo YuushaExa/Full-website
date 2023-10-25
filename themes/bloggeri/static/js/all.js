12041c87dafffeba0d9e783773225c4f2205a1a8
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-10-25 22:21:37 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -5,9 +5,9 @@ let t,e;const n=new Set,o=document.createElement("link"),s=o.relList&&o.relList.
    delay: [1000, null],
       });
 
-$('.card').hover(function (event) { 
+$('.category').hover(function (event) { 
     event.preventDefault(); 
-    $('.category').append('<span class="toggleButton" aria-label="Favorites" onclick="toggleLocalStorage(event)"><svg xmlns="http://www.w3.org/2000/svg" height="24" class="toggleButtonSVG" viewBox="0 -960 960 960" width="24"><path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Z"/></svg></span>');
+    $(this).append('<span class="toggleButton" aria-label="Favorites" onclick="toggleLocalStorage(event)"><svg xmlns="http://www.w3.org/2000/svg" height="24" class="toggleButtonSVG" viewBox="0 -960 960 960" width="24"><path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Z"/></svg></span>');
  }, function() {
     $('.toggleButton').remove();
 });
