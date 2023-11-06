d4749fe5534baae51bdde6de356be708a38992c8
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-11-06 20:53:40 +0300

    Update lightboxAI.js

--- themes/bloggeri/static/js/lightboxAI.js
+++ themes/bloggeri/static/js/lightboxAI.js
@@ -1,4 +1,4 @@
-var galleryImages = document.querySelectorAll('.cover-front img');
+var galleryImages = document.querySelectorAll('img');
 var currentIndex = 0;
 var totalImages = galleryImages.length;
 var openedImageCounter = document.getElementById('opened-image-counter');
@@ -19,7 +19,7 @@ galleryImages.forEach(function(image) {
   link.appendChild(image);
 });
 
-var gallery = document.querySelector('.cover-front');
+var gallery = document.querySelector('img');
 
 // Get the lightbox-content element
 var lightboxContent = document.querySelector('.thumbnails-container');
