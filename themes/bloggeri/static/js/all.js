453834f69ac40374aa34037e46d6de8398ca3d7f
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-12-20 18:36:21 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -389,3 +389,58 @@ if (event.target == modal9) {
 closeLightbox();
 };
 };
+
+document.body.addEventListener("click", async function(event) {
+  var target = event.target;
+ var closestLink = target.closest(".navbar-start a, .navbar-brand a");
+  if (closestLink) {
+    event.preventDefault();
+
+    try {
+      const response = await fetch(closestLink.href);
+      const html = await response.text();
+
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(html, 'text/html');
+
+      const postTitle = doc.querySelector('main').innerHTML;
+
+      const mainElement = document.querySelector('main');
+      mainElement.innerHTML = postTitle;
+
+      window.history.pushState({}, "", closestLink.href);
+
+       var cardContainer = document.getElementById('cardContainer');
+  if (cardContainer) {
+    displaySavedCards();
+ var toggleButtons = document.querySelectorAll('.toggleButton path');
+toggleButtons.forEach(function(button) {
+  button.style.fill = 'unset';
+});
+var toggleButtons1 = document.querySelectorAll('.toggleButtonSVG');
+toggleButtons1.forEach(function(button1) {
+  button1.style.fill = 'rgb(4 252 14 / 80%)';
+});
+  var totalCountfav = document.getElementById('totalCount-fav');
+  if (totalCountfav) {
+document.getElementById("totalCount-fav").textContent = "" + totalCount;
+   updateCounter();
+ }
+   
+  }
+
+var toggleButton12 = document.querySelectorAll('.toggleButton');
+  if (toggleButton12) {
+    attachToggleListeners();
+}
+
+ tippy('.toggleButton', {
+        content: (reference) => reference.getAttribute('aria-label'),
+   delay: [1000, null],
+      });
+     
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+});
