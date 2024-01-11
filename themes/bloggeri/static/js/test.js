edf553523fa7b388bb8106d7bc60da93f5d03291
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-01-12 00:57:07 +0300

    Update test.js

--- themes/bloggeri/static/js/test.js
+++ themes/bloggeri/static/js/test.js
@@ -14,5 +14,5 @@ $('html').css({
          });
 var bg = 'rgb('+ thisColor +')'
 var bg_url = document.querySelector("img").src;
-document.querySelector(".cover").style.backgroundImage = "linear-gradient(-180deg,#1b1d1e8c 0," + bg + "," + bg + " 100%),url('" + bg_url + "')";
+document.querySelector(".cover").style.backgroundImage = "url('" + bg_url + "')";
 });
