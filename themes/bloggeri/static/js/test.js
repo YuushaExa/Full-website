4020b139398601a8efbb0a85bc129e0523e64fb6
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-01-17 01:16:11 +0300

    Update test.js

--- themes/bloggeri/static/js/test.js
+++ themes/bloggeri/static/js/test.js
@@ -13,7 +13,9 @@ $('html').css({
             background: 'rgb('+ thisColor +')'
          });
 var bg = 'rgb('+ thisColor +')'
+var bg1 = 'rgb('+ thisColor +', 0.1)'
 var bg_url = document.querySelector("img").src;
-document.querySelector(".cover").style.backgroundImage = `linear-gradient(to top, ${bg} 0, rgba(20, 38, 51, 0.1) 60%), url('${bg_url}')`;    document.querySelector(".cover").style.backgroundPosition = "center";
+document.querySelector(".cover").style.backgroundImage = `linear-gradient(to top, ${bg} 0, ${bg1} 60%), url('${bg_url}')`;    
+document.querySelector(".cover").style.backgroundPosition = "center";
 document.querySelector(".cover").style.backgroundSize = "cover";
 });
