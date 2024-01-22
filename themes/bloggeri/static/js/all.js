9cddfd920cbcd092cefbd876bb2d4aa9613d3276
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-01-22 23:59:04 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -12,44 +12,54 @@ let t,e;const n=new Set,o=document.createElement("link"),s=o.relList&&o.relList.
 
 // played 
 document.addEventListener('DOMContentLoaded', function() {
-  function handleCardClick(sectionClass, storageKey) {
-    var cardsContainers = document.querySelectorAll(sectionClass);
-    var cardData = [];
+function handleCardClick(sectionClass, storageKey) {
+var cardsContainers = document.querySelectorAll(sectionClass);
+var cardData = [];
 
-    var storedData = localStorage.getItem(storageKey);
-    if (storedData) {
-      cardData = JSON.parse(storedData);
-    }
+var storedData = localStorage.getItem(storageKey);
+if (storedData) {
+  cardData = JSON.parse(storedData);
+}
 
-    cardsContainers.forEach(function(cardsContainer) {
-      cardsContainer.addEventListener('click', function(event) {
-        var card = event.target.closest('.card');
-        if (card) {
-          var title = card.querySelector('.title').textContent;
-          var image = card.querySelector('.card-image img').src;
-          var strippedImage = decodeURIComponent(image.substring(image.indexOf('=') + 1, image.indexOf('&')));
-          var href = card.querySelector('.card-image').href;
+cardsContainers.forEach(function(cardsContainer) {
+  cardsContainer.addEventListener('click', function(event) {
+    var card = event.target.closest('.card');
+    if (card) {
+      var title = card.querySelector('.title').textContent;
+      var image = card.querySelector('.card-image img').src;
+      var strippedImage = decodeURIComponent(image.substring(image.indexOf('=') + 1, image.indexOf('&')));
+      var href = card.querySelector('.card-image').href;
+      var currentDate = new Date();
+      var options = { weekday: 'short', day: 'numeric', month: 'short', year: 'numeric' };
+      var formattedDate = currentDate.toLocaleDateString('en-US', options);
 
-          var indexToRemove = -1;
-          cardData.forEach(function(item, index) {
-            if (item.title === title && item.image === strippedImage && item.href === href) {
-              indexToRemove = index;
-            }
-          });
-
-          if (indexToRemove !== -1) {
-            cardData.splice(indexToRemove, 1);
-            var jsonData = JSON.stringify(cardData);
-            localStorage.setItem(storageKey, jsonData);
-          }
-        }
+      var isDuplicate = cardData.some(function(item) {
+        return item.title === title && item.image === strippedImage && item.href === href;
       });
-    });
-  }
 
-  handleCardClick('.Backlog', 'Backlog');
-  handleCardClick('.Completed', 'Completed');
-  handleCardClick('.Playing', 'Playing');
+      if (!isDuplicate) {
+        var data = {
+          "title": title,
+          "image": strippedImage,
+          "href": href,
+          "dateAdded": formattedDate
+        };
+
+        cardData.push(data);
+        var jsonData = JSON.stringify(cardData);
+        localStorage.setItem(storageKey, jsonData);
+      }
+    }
+  });
+});
+}
+
+handleCardClick('.Playing', 'Playing');
+handleCardClick('.Backlog', 'Backlog');
+handleCardClick('.Completed', 'Completed');
+handleCardClick('.OnHold', 'OnHold');
+handleCardClick('.Dropped', 'Dropped');
+handleCardClick('.Wishlist', 'Wishlist');
 });
 // history
 
