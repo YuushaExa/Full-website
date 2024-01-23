c28c29500ffe277e26d7c7f4ac209caca11ff1b1
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-01-24 01:06:09 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -59,7 +59,7 @@ divElement.parentNode.removeChild(divElement);
 });
 
 // played 
-$(document).on("click", ".AddList", function(event) {
+$(document).on("click", "#Listcontainer", function(event) {
   function handleCardClick(sectionClass, storageKey) {
     var cardsContainers = document.querySelectorAll(sectionClass);
     var cardData = [];
