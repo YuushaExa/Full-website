27ae77fc0b56e1e9acf7f889941e583dc20b1ce1
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-01-30 02:10:13 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -257,6 +257,18 @@ var existingRating = existingRatingData.find(function(item) {
 // Update the rating display with the current rating
 ratingDisplay.textContent = "Current Rating: " + (existingRating ? existingRating.rating : "Not rated");
 //
+
+  const stars = document.querySelectorAll('.star');
+
+stars.forEach(star => {
+  star.addEventListener('mouseover', () => {
+    star.classList.add('hover');
+  });
+
+  star.addEventListener('mouseout', () => {
+    star.classList.remove('hover');
+  });
+});
 });  
 // test
 
