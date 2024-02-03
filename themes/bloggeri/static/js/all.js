ade564918727c34c9e0ba20049697d1683f8ca45
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-02-04 00:24:49 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -338,9 +338,22 @@ textArea.value = savedNoteText || "";
             notes[href] = noteText;
             localStorage.setItem("Notes", JSON.stringify(notes));
 
+          var storedNotes = localStorage.getItem("Notes");
+if (storedNotes) {
+var notesData = JSON.parse(storedNotes);
+
+var cards = document.querySelectorAll('.card');
+
+cards.forEach(function(card) {
+var href = card.querySelector('.card-image').href;
+
+if (notesData[href]) {
   var notesReadDiv = document.createElement('div');
   notesReadDiv.className = 'NotesRead';
   card.appendChild(notesReadDiv);
+}
+});
+}
 
         });
 
