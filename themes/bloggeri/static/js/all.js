4d8327f0712c5e0d4afd64ee7d25e937534b34c3
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-03-04 04:25:54 +0300

    Update all.js

--- themes/bloggeri/static/js/all.js
+++ themes/bloggeri/static/js/all.js
@@ -219,40 +219,40 @@ const trackedLocalStorage = {
 
 
 function handleCardClick(sectionClass, storageKey) {
-  var cardsContainers = document.querySelectorAll(sectionClass);
-  var cardData = [];
+    var cardsContainers = document.querySelectorAll(sectionClass);
+    var cardData = [];
  
-  var storedData = LocalStorage.getItem(storageKey);
-  if (storedData) {
-    cardData = JSON.parse(storedData);
-  }
+    var storedData = localStorage.getItem(storageKey);
+    if (storedData) {
+      cardData = JSON.parse(storedData);
+    }
  
-  cardsContainers.forEach(function(cardsContainer) {
-    cardsContainer.addEventListener('click', function(event) {
-      var card = event.target.closest('.card');
-      if (card) {
-        var title = card.querySelector('.title').textContent;
-        var image = card.querySelector('.card-image img').src;
-        var strippedImage = decodeURIComponent(image.substring(image.indexOf('=') + 1, image.indexOf('&')));
-        var href = card.querySelector('.card-image').href;
-        var currentDate = new Date();
-        var options = { weekday: 'short', day: 'numeric', month: 'short', year: 'numeric' };
-        var formattedDate = currentDate.toLocaleDateString('en-US', options);
-
-        var isDuplicate = cardData.some(function(item) {
-          return item.title === title && item.image === strippedImage && item.href === href;
-        });
-
-        if (!isDuplicate) {
-          var data = {
-            "title": title,
-            "image": strippedImage,
-            "href": href,
-            "dateAdded": formattedDate
-          };
-
-          cardData.push(data);
-          var jsonData = JSON.stringify(cardData);
+    cardsContainers.forEach(function(cardsContainer) {
+      cardsContainer.addEventListener('click', function(event) {
+        var card = event.target.closest('.card');
+        if (card) {
+          var title = card.querySelector('.title').textContent;
+          var image = card.querySelector('.card-image img').src;
+          var strippedImage = decodeURIComponent(image.substring(image.indexOf('=') + 1, image.indexOf('&')));
+          var href = card.querySelector('.card-image').href;
+          var currentDate = new Date();
+          var options = { weekday: 'short', day: 'numeric', month: 'short', year: 'numeric' };
+          var formattedDate = currentDate.toLocaleDateString('en-US', options);
+ 
+          var isDuplicate = cardData.some(function(item) {
+            return item.title === title && item.image === strippedImage && item.href === href;
+          });
+ 
+          if (!isDuplicate) {
+            var data = {
+              "title": title,
+              "image": strippedImage,
+              "href": href,
+              "dateAdded": formattedDate
+            };
+ 
+            cardData.push(data);
+            var jsonData = JSON.stringify(cardData);
           trackedLocalStorage.setItem(storageKey, jsonData);
         } else {
           // Remove the duplicate item from cardData array
