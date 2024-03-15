1bf68176dc7e9bc5e32e6a2aa44d3f00b650e8a2
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-03-16 02:56:22 +0300

    Update test.js

--- themes/bloggeri/static/js/test.js
+++ themes/bloggeri/static/js/test.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
+var colorThief = new ColorThief();
+$('.cover-front').each(function() {
+    var thumb = $(this);
+    thumb.find('img').each(function() {
+        thisColor = colorThief.getColor(this);
+        thumb.parent().find('.cover').css({
+         })
+    });
+});
+$('html').css({
+         });
+var bg = 'rgb('+ thisColor +')'
 
 var bg_url = document.querySelector("img").src;
 document.querySelector(".cover").style.backgroundImage = `linear-gradient(to top, rgb(22, 24, 28) 0, rgb(22 24 28 / 10%) 60%), url('${bg_url}')`;    
