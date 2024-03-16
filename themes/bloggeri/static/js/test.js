e74e976404acd8d5a2d133ee7e4d759d704285b9
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-03-16 03:03:11 +0300

    Update test.js

--- themes/bloggeri/static/js/test.js
+++ themes/bloggeri/static/js/test.js
@@ -1,5 +1,4 @@
 setTimeout(function () {
-document.addEventListener('DOMContentLoaded', function() {
 var colorThief = new ColorThief();
 $('.cover-front').each(function() {
     var thumb = $(this);
@@ -17,5 +16,4 @@ var bg_url = document.querySelector("img").src;
 document.querySelector(".cover").style.backgroundImage = `linear-gradient(to top, rgb(22, 24, 28) 0, rgb(22 24 28 / 10%) 60%), url('${bg_url}')`;    
 document.querySelector(".cover").style.backgroundPosition = "center";
 document.querySelector(".cover").style.backgroundSize = "cover";
-});
 }, 1000)
