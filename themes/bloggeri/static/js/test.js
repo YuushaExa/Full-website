ea9c432a4f504ab3398acf84cccba9f789e6e089
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-03-28 23:41:55 +0300

    Update test.js

--- themes/bloggeri/static/js/test.js
+++ themes/bloggeri/static/js/test.js
@@ -13,10 +13,44 @@ $('html').css({
 // var bg = 'rgb('+ thisColor +')'
 
 var images = Array.from(document.querySelectorAll("img"));
-var randomIndex = Math.floor(Math.random() * images.length);
-var randomImage = images[randomIndex];
-var randomImageUrl = randomImage.src;
-var bg_url = randomImageUrl;
+var randomImage, randomImageUrl;
+
+// Check if the image URL returns a 404 error
+function checkImageAvailability(imageUrl) {
+  return new Promise(function(resolve, reject) {
+    var img = new Image();
+    img.onload = function() {
+      // Image exists
+      resolve(imageUrl);
+    };
+    img.onerror = function() {
+      // Image doesn't exist
+      reject();
+    };
+    img.src = imageUrl;
+  });
+}
+
+// Select a random image URL that is available
+function getRandomImageUrl() {
+  var randomIndex = Math.floor(Math.random() * images.length);
+  randomImage = images[randomIndex];
+  randomImageUrl = randomImage.src;
+
+  return checkImageAvailability(randomImageUrl)
+    .then(function(validImageUrl) {
+      bg_url = validImageUrl;
+      // Use the valid random image URL (bg_url) in your code
+    })
+    .catch(function() {
+      // Retry with another random image URL
+      return getRandomImageUrl();
+    });
+}
+
+// Start the process
+getRandomImageUrl();
+    
 document.querySelector(".cover").style.backgroundImage = `linear-gradient(to top, rgb(22, 24, 28) 0, rgb(22 24 28 / 10%) 60%), url('${bg_url}')`;    
 document.querySelector(".cover").style.backgroundPosition = "center";
 document.querySelector(".cover").style.backgroundSize = "cover";
