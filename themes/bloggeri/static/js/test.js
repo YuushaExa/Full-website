57b1ce30b9d624fde9905fe07f0dae9e4cc8afda
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-03-28 23:37:36 +0300

    Update test.js

--- themes/bloggeri/static/js/test.js
+++ themes/bloggeri/static/js/test.js
@@ -14,7 +14,9 @@ $('html').css({
 
 var images = Array.from(document.querySelectorAll("img"));
 var randomIndex = Math.floor(Math.random() * images.length);
-var bg_url = images[randomIndex];
+var randomImage = images[randomIndex];
+var randomImageUrl = randomImage.src;
+var bg_url = randomImageUrl;
 document.querySelector(".cover").style.backgroundImage = `linear-gradient(to top, rgb(22, 24, 28) 0, rgb(22 24 28 / 10%) 60%), url('${bg_url}')`;    
 document.querySelector(".cover").style.backgroundPosition = "center";
 document.querySelector(".cover").style.backgroundSize = "cover";
