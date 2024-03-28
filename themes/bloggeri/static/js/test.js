f1a2fb65ee24310f333e1444353e4552d05dc034
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-03-28 23:49:15 +0300

    Update test.js

--- themes/bloggeri/static/js/test.js
+++ themes/bloggeri/static/js/test.js
@@ -52,7 +52,9 @@ function getRandomImageUrl() {
 // Start the process
 getRandomImageUrl();
     
-document.querySelector(".cover").style.backgroundImage = "linear-gradient(to top, rgb(22, 24, 28) 0, rgb(22 24 28 / 10%) 60%), url('" + bg_url + "')";
-document.querySelector(".cover").style.backgroundPosition = "center";
+getRandomImageUrl().then(function() {
+  document.querySelector(".cover").style.backgroundImage = `linear-gradient(to top, rgb(22, 24, 28) 0, rgb(22 24 28 / 10%) 60%), url('${bg_url}')`;
+});
+    document.querySelector(".cover").style.backgroundPosition = "center";
 document.querySelector(".cover").style.backgroundSize = "cover";
 }, 1000)
