9541ea513c66056910feee612953d0506aa34b9e
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-04-06 00:25:16 +0300

    Update test.js

--- themes/bloggeri/static/js/test.js
+++ themes/bloggeri/static/js/test.js
@@ -69,7 +69,7 @@ function getRandomImageUrl() {
 getRandomImageUrl();
     
 getRandomImageUrl().then(function() {
-  document.querySelector(".cover").style.backgroundImage = `linear-gradient(to top, rgb(22, 24, 28) 0, rgb(22 24 28 / 10%) 60%), url('${bg_url}')`;
+  document.querySelector(".cover").style.backgroundImage = `linear-gradient(to top, rgb(22, 24, 28) 0, rgb(22 24 28 / 10%) 60%), url('${src}')`;
 });
     document.querySelector(".cover").style.backgroundPosition = "center";
 document.querySelector(".cover").style.backgroundSize = "cover";
