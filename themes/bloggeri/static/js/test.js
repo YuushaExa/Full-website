cf3b18aa4e5709b711d7baadde0de3efc59a4d0d
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-04-06 01:10:19 +0300

    Update test.js

--- themes/bloggeri/static/js/test.js
+++ themes/bloggeri/static/js/test.js
@@ -13,8 +13,9 @@ $('html').css({
 // var bg = 'rgb('+ thisColor +')'
 
 var images = Array.from(document.querySelectorAll(".screenshots1 img"));
-var randomImage, randomImageUrl;
 var bg_url; // Declare bg_url variable
+var MAX_RETRY_ATTEMPTS = 5;
+var retryAttempts = 0;
 
 // Check if the image URL returns a 404 error
 function checkImageAvailability(imageUrl) {
@@ -32,36 +33,28 @@ function checkImageAvailability(imageUrl) {
   });
 }
 
-// Modify the source URL if it matches the specific format
-function modifyImageUrl(imageUrl) {
-  var regex = /https:\/\/wsrv.nl\/\?url=(.*?)&/; // Regular expression to match the format
-  var match = imageUrl.match(regex);
-  if (match && match.length > 1) {
-    return match[1]; // Return the extracted URL
-  }
-  return imageUrl; // Return the original URL if no match found
-}
-
 // Select a random image URL that is available
 function getRandomImageUrl() {
   var randomIndex = Math.floor(Math.random() * images.length);
-  randomImage = images[randomIndex];
-  randomImageUrl = randomImage.src;
-  randomImageUrl = modifyImageUrl(randomImageUrl); // Modify the URL if necessary
+  var randomImage = images[randomIndex];
+  var randomImageUrl = randomImage.src;
   return checkImageAvailability(randomImageUrl)
     .then(function(validImageUrl) {
       bg_url = validImageUrl;
       // Use the valid random image URL (bg_url) in your code
     })
     .catch(function() {
-      // Retry with another random image URL
-      return getRandomImageUrl();
+      if (retryAttempts < MAX_RETRY_ATTEMPTS) {
+        retryAttempts++;
+        return getRandomImageUrl();
+      } else {
+        // Handle the case when the maximum retry attempts are reached
+        console.log("Max retry attempts reached. Unable to find a valid image.");
+      }
     });
 }
 
 // Start the process
-getRandomImageUrl();
-
 getRandomImageUrl().then(function() {
   document.querySelector(".cover").style.backgroundImage = `linear-gradient(to top, rgb(22, 24, 28) 0, rgb(22 24 28 / 10%) 60%), url('${bg_url}')`;
 });
