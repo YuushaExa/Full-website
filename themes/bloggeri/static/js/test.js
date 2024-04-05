3254bbb9e14de37bf25fa2f02b85ab16e570e3f1
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-04-06 00:35:56 +0300

    Update test.js

--- themes/bloggeri/static/js/test.js
+++ themes/bloggeri/static/js/test.js
@@ -16,53 +16,7 @@ var images = Array.from(document.querySelectorAll("img"));
 var randomImage, randomImageUrl;
 var bg_url; // Declare bg_url variable
 
-function checkImageAvailability(imageUrl) {
-  return new Promise(function(resolve, reject) {
-    var img = new Image();
-    img.onload = function() {
-      // Image exists
-      resolve(imageUrl);
-    };
-    img.onerror = function() {
-      // Image doesn't exist
-      reject();
-    };
-    img.src = imageUrl;
-  });
-}
 
-// Select a random image URL that is available
-function getRandomImageUrl() {
-  var screenshots1Elements = document.getElementsByClassName("screenshots1");
-
-  var screenshots1Images = Array.from(screenshots1Elements).map(function(element) {
-    var src = element.src;
-
-    return src;
-  });
-
-  if (screenshots1Images.length > 0) {
-    var randomIndex = Math.floor(Math.random() * screenshots1Images.length);
-    var randomImageUrl = screenshots1Images[randomIndex];
-
-    return checkImageAvailability(randomImageUrl)
-      .then(function(validImageUrl) {
-        bg_url = validImageUrl;
-        // Use the valid random image URL (bg_url) in your code
-      })
-      .catch(function() {
-        // Retry with another random image URL
-        return getRandomImageUrl();
-      });
-  } else {
-    // No images found in elements with class "screenshots1"
-    // Search for other sources or handle the case accordingly
-    return Promise.reject("No images found");
-  }
-}
-
-// Start the process
-getRandomImageUrl();
 getRandomImageUrl().then(function() {
   document.querySelector(".cover").style.backgroundImage = `linear-gradient(to top, rgb(22, 24, 28) 0, rgb(22 24 28 / 10%) 60%), url('${bg_url}')`;
 });
