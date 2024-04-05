356c9f0929a910a9604913a37b7630f6c2734017
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-04-06 00:38:42 +0300

    Update test.js

--- themes/bloggeri/static/js/test.js
+++ themes/bloggeri/static/js/test.js
@@ -16,10 +16,31 @@ var images = Array.from(document.querySelectorAll("img"));
 var randomImage, randomImageUrl;
 var bg_url; // Declare bg_url variable
 
+const screenshots = document.getElementsByClassName("screenshots1");
 
-getRandomImageUrl().then(function() {
-  document.querySelector(".cover").style.backgroundImage = `linear-gradient(to top, rgb(22, 24, 28) 0, rgb(22 24 28 / 10%) 60%), url('${bg_url}')`;
-});
+// Create an array to store the image URLs
+const bg_url = [];
+
+// Loop through each element and extract the image URLs
+for (let i = 0; i < screenshots.length; i++) {
+  const screenshot = screenshots[i];
+
+  // Check if the element is an image
+  if (screenshot.tagName === "IMG") {
+    const imageUrl = screenshot.src;
+    bg_url.push(imageUrl);
+  }
+}
+
+// Function to generate a random index
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+// Get a random image URL from bg_url array
+const randomImageUrl = bg_url[getRandomIndex(bg_url)];
+    
+  document.querySelector(".cover").style.backgroundImage = `linear-gradient(to top, rgb(22, 24, 28) 0, rgb(22 24 28 / 10%) 60%), url('${randomImageUrl}')`;
     document.querySelector(".cover").style.backgroundPosition = "center";
 document.querySelector(".cover").style.backgroundSize = "cover";
 }, 1000)
